510dd0809151dfaf1e53969bfa5e5e43ed6f5b97
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2024-07-31 23:14:32 +0530

    Time: 27 ms (79.08%) | Memory: 36 MB (5.18%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -34,13 +34,54 @@ public:
 
         return dp[i][j] =  option1 || option2 || option3;
     }
+
+    bool isPossible(string s,int i,int m) {
+
+        for(int j = i ; j < m ; j++) {
+            if(s[j] != '*') return false;
+        }
+        return true;
+    }
+    bool tabulation(string s,string p) {
+        int n = s.length();
+        int m = p.length();
+
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+
+        for(int i = 0 ; i < m ; i++) {
+            dp[n][i] = isPossible(p,i,m);
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            dp[i][m] = false;
+        }
+        dp[n][m] = true;
+
+        for(int i = n-1 ; i >= 0 ; i--) {
+            for(int j = m-1 ; j >= 0 ; j--) {
+
+                if(s[i] == p[j]) {
+                    dp[i][j] = dp[i+1][j+1];
+                } else if(p[j] == '?') {
+                    dp[i][j] = dp[i+1][j+1];
+                } else if(p[j] == '*') {
+                    dp[i][j] = dp[i+1][j] || dp[i][j+1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[0][0];
+
+    }
     bool isMatch(string s, string p) {
         
         int n = s.length();
-        int m = p.length();
+        // int m = p.length();
         
-        int i = 0, j = 0;
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        return dfs(s,p,i,j,n,m,dp);
+        // int i = 0, j = 0;
+        // vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        // return dfs(s,p,i,j,n,m,dp);
+        return tabulation(s,p);
     }
 };
