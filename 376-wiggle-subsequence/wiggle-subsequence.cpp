c33e6c3c7ce97ae672a8cae129f350ab714ff72f
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2024-09-23 17:45:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (26.76%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -6,7 +6,7 @@ public:
 
         int notPick = 0 + func(nums,i+1,sign,n,dp);
         int pick = 0;
-
+    
         if(sign == 0) {
             if(nums[i]-nums[i-1] != 0)
                 pick = 1 + func(nums,i+1,(nums[i]-nums[i-1] < 0) ? 2 : 1,n,dp);
@@ -21,14 +21,31 @@ public:
     }
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(3,-1));
-        // dp[0][0] = 1;
-        // dp[0][1] = 1;
+        // vector<vector<int>> dp(n,vector<int>(3,-1));
+        // return func(nums,0,0,n,dp);
 
-        // for(int i = 1 ; i < n ; i++) {
+        vector<vector<int>> dp(n,vector<int>(2,0)); // 0 -> positive and 1 -> negative
 
-        // }
-        // return max(dp[n-1][0],dp[n-1][1]);
-        return func(nums,1,0,n,dp) + 1;
+        for(int i = 1 ; i < n ; i++) {
+            int pick = 0;
+            int notPick = 0;
+
+            if(nums[i]-nums[i-1] > 0) {
+                pick = 1 + dp[i-1][1];
+                notPick = 0 + dp[i-1][0];
+                dp[i][0] = max(pick,notPick);
+            } else if(nums[i]-nums[i-1] < 0){
+                pick = 1 + dp[i-1][0];
+                notPick = 0 + dp[i-1][1];
+                dp[i][1] = max(pick,notPick);
+            } else {
+                dp[i][0] = dp[i-1][0];
+                dp[i][1] = dp[i-1][1];
+            }
+        }
+        for(int i = 0 ; i < n ; i++) {
+            cout << dp[i][0] << "  :  " << dp[i][1] << endl;
+        }
+        return 1 + max(dp[n-1][0],dp[n-1][1]);
     }
 };
