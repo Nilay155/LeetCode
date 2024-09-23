ac4e7f39969839462b8c28a30589ac4445d2083f
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2024-09-23 19:03:09 +0530

    Time: 16 ms (25.47%) | Memory: 17.1 MB (43.23%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -27,7 +27,26 @@ public:
             arr.push_back(it.first);
         }
         int m = arr.size();
-        vector<int> dp(m+1,-1);
-        return solve(arr,0,m,dp);
+        // vector<int> dp(m+1,-1);
+        // return solve(arr,0,m,dp);
+
+        vector<int> dp(m,0);
+        dp[0] = mpp[arr[0]]*arr[0];
+
+        for(int i = 1 ; i < m ; i++) {
+
+            int pick = 0;
+            int notPick = 0;
+
+            if(arr[i]-1 != arr[i-1]) {
+                pick = mpp[arr[i]]*arr[i] + dp[i-1];
+                notPick = 0 + dp[i-1];
+            } else {
+                pick = mpp[arr[i]]*arr[i] + ((i >= 2) ? dp[i-2] : 0);
+                notPick = 0 + dp[i-1];
+            }
+            dp[i] = max(pick,notPick);
+        }
+        return dp[m-1];
     }
 };
