5381b6b172f01d39a4e4a8849a9541c9ce6d2f2c
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2024-10-31 13:42:40 +0530

    Time: 79 ms (55.17%) | Memory: 19.8 MB (48.13%) - LeetSync

---
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // map<pair<int,vector<int>>,int> mpp;
+    // unordered_map<int,int> map;
+    long long dp[101][101][101];
+    long long solve(vector<int> &robot,vector<int> &distance,vector<int> &limit,int i,int j,int cnt,int &n,int &m) {
+        if(i >= n) return 0; // Repairing Done For All Robots
+        if(j >= m) return LLONG_MAX;
+        if(dp[i][j][cnt] != -1) return dp[i][j][cnt];
+        // if(mpp.find({i,limit}) != mpp.end()) return mpp[{i,limit}];
+
+        long long op1 = LLONG_MAX;
+        long long op2 = 0 + solve(robot,distance,limit,i,j+1,0,n,m);
+
+        if(cnt < limit[j]) {
+            long long mini = abs(distance[j]-robot[i]);
+            long long rec = solve(robot,distance,limit,i+1,j,cnt+1,n,m);
+            if(rec != LLONG_MAX)
+                op1 = min(op1, mini + rec);
+        }
+        return dp[i][j][cnt] = min(op1,op2);
+    }
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        int n = robot.size();
+        int m = factory.size();
+
+        sort(robot.begin(),robot.end());
+        sort(factory.begin(),factory.end());
+        vector<int> limit,distance;
+        for(int i = 0 ; i < m ; i++) {
+            limit.push_back(factory[i][1]);
+            distance.push_back(factory[i][0]); 
+        }
+        memset(dp,-1,sizeof(dp));
+        return solve(robot,distance,limit,0,0,0,n,m);
+    }
+};
