56f182241404045586807184f2a65a120bfb489c
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2024-11-10 02:57:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (61.39%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+    int dp[21][21][2];
     int solve(vector<int> &nums,int left,int right,int turn) {
         if(left >= right) return (turn == 0) ? -nums[left] : nums[left];
+        if(dp[left][right][turn] != -1) return dp[left][right][turn];
 
         int score = 0;
         if(turn) {
@@ -12,10 +14,11 @@ public:
                         -nums[right] + solve(nums,left,right-1,1));
         }
         // cout << score << endl;
-        return score;
+        return dp[left][right][turn] = score;
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        memset(dp,-1,sizeof(dp));
         // cout << solve(nums,0,n-1,1) << endl;
         return (solve(nums,0,n-1,1) >= 0);
     }
