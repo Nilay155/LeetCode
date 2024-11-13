612f70e2bcdcd228450ab2cb821e6713253a9fec
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2024-11-13 15:54:12 +0530

    Time: 2 ms (63.88%) | Memory: 12.1 MB (30.25%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     // int dp[3001][3001];
-    int solve(vector<int> &nums,int i,int disc1,int disc2,int &n,vector<vector<int>> &dp) {
-        if(i >= n) return abs(disc1-disc2);
-        if(dp[disc1][disc2] != -1) return dp[disc1][disc2];
+    int solve(vector<int> &nums,int i,int disc,int sum,int &n,vector<vector<int>> &dp) {
+        if(i >= n) return abs(sum-disc-disc);
+        if(dp[i][disc] != -1) return dp[i][disc];
 
-        int op1 = solve(nums,i+1,disc1+nums[i],disc2,n,dp);
-        int op2 = solve(nums,i+1,disc1,disc2+nums[i],n,dp);
+        int op1 = solve(nums,i+1,disc+nums[i],sum,n,dp);
+        int op2 = solve(nums,i+1,disc,sum,n,dp);
 
-        return dp[disc1][disc2] = min(op1,op2);
+        return dp[i][disc] = min(op1,op2);
     }
     int lastStoneWeightII(vector<int>& stones) {
         int n = stones.size();
@@ -16,7 +16,7 @@ public:
         // memset(dp,-1,sizeof(dp));
         int sum = 0;
         for(int num : stones) sum += num;
-        vector<vector<int>> dp(sum+1,vector<int>(sum+1,-1));
-        return solve(stones,0,0,0,n,dp);
+        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+        return solve(stones,0,0,sum,n,dp);
     }
 };
