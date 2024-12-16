10601000ade3b6396e43d0325c4e29f3df2def05
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2024-12-16 15:58:27 +0530

    Time: 31 ms (23.95%) | Memory: 12.5 MB (38.15%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maximumLength(string s) {
+        int n = s.length();
+
+        unordered_map<string,int> mpp;
+
+        for(int i = 0 ; i < n ; i++) {
+            string temp;
+            temp.push_back(s[i]);
+            mpp[temp]++;
+            for(int j = i+1 ; j < n ; j++) {
+                if(temp.back() == s[j]) {
+                    temp.push_back(s[j]);
+                    mpp[temp]++;
+                } else {
+                    break;
+                }
+
+            }
+        }
+
+        int ans = -1;
+
+        for(auto& it : mpp) {
+            if(it.second >= 3) {
+                
+                string temp = it.first;
+                int len = temp.size();
+                cout << temp << endl;
+                ans = max(ans,len);
+            }
+        }
+        return ans;
+    }
+};
