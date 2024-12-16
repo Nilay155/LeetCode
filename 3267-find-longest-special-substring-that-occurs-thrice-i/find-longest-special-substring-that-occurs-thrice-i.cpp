4251579ea691ae88204f85cb3b1c70ae217b3426
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2024-12-16 22:10:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (70.34%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
+    bool check(vector<int> &arr) {
+        int flag = 0;
+        for(int i = 0 ; i < 26 ; i++) {
+            if(arr[i] > 0 && flag) return false;
+            if(arr[i] > 0) flag = 1;
+        }
+        return true;
+    }
+    bool isPossible(string &s,int k) {
+        vector<int> count(26);
+        vector<int> temp(26);
+
+        int i = 0, j = 0;
+        int n = s.length();
+
+        while(j < n) {
+            temp[s[j]-'a']++;
+            if((j-i+1)%k == 0) {
+                if(check(temp)) {
+                    count[s[j]-'a']++;
+                }
+                temp[s[i]-'a']--;
+                i++;
+            }
+            j++;
+        }
+
+        for(int i = 0 ; i < 26 ; i++) {
+            if(count[i] > 2) return true;
+        }
+        return false;
+    }
     int maximumLength(string s) {
         int n = s.length();
 
-        unordered_map<string,int> mpp;
+        int start = 1;
+        int end = n;
 
-        for(int i = 0 ; i < n ; i++) {
-            string temp;
-            temp.push_back(s[i]);
-            mpp[temp]++;
-            for(int j = i+1 ; j < n ; j++) {
-                if(temp.back() == s[j]) {
-                    temp.push_back(s[j]);
-                    mpp[temp]++;
-                } else {
-                    break;
-                }
-
+        while(start <= end) {
+            int mid = (start + end) >> 1;
+            if(isPossible(s,mid)) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
-
-        int ans = -1;
-
-        for(auto& it : mpp) {
-            if(it.second >= 3) {
-                
-                string temp = it.first;
-                int len = temp.size();
-                // cout << temp << endl;
-                ans = max(ans,len);
-            }
-        }
-        return ans;
+        return (end == 0) ? -1 : end;
     }
 };
