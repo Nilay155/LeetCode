95170da35d46d9eecf3d42e725687f3cc7f1f4bf
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-01-18 18:26:51 +0530

    Time: 3 ms (53.95%) | Memory: 89.3 MB (74.17%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -7,7 +7,7 @@ public:
         long long int totalSum = 0;
         for(int num : nums) totalSum += num;
 
-        long long int currSum = 0;
+        long long int  currSum = 0;
         for(int i = 0 ; i < n-1 ; i++) {
             currSum += nums[i];
             long long int remSum = totalSum - currSum;
