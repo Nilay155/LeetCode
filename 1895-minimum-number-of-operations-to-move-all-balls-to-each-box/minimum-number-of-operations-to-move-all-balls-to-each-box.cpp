a22ed3f0d9a917a47617b067c5e2d473b56c9463
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-01-18 17:35:04 +0530

    Time: 3 ms (51.21%) | Memory: 12 MB (96.65%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
-        vector<int> ones;
+        vector<int> ans(n);
 
+        int res = 0;
+        int leftOnes = 0;
         for(int i = 0 ; i < n ; i++) {
-            if(boxes[i] == '1') ones.push_back(i);
+            res += leftOnes;
+            if(boxes[i] == '1') {
+                leftOnes += 1;
+            }
+            ans[i] += res;
         }
 
-        vector<int> ans;
-        int m = ones.size();
-        for(int i = 0 ; i < n ; i++) {
-            
-            int res = 0;
-            for(int j = 0 ; j < m ; j++) {
-                if(i == ones[j]) continue;
-                res += abs(i-ones[j]);
+        res = 0;
+        int rightOnes = 0;
+        for(int i = n-1 ; i >= 0 ; i--) {
+            res += rightOnes;
+            if(boxes[i] == '1') {
+                rightOnes += 1;
             }
-            ans.push_back(res);
+            ans[i] += res;
         }
         return ans;
     }
