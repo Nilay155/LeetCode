36c05b1a5c81ad6230e897a432ea2915ff543930
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-01-23 23:54:32 +0530

    Time: 7 ms (4.63%) | Memory: 8.2 MB (13.96%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int func(int n,int turn) {
-        
-        int ans = 1;
-        for(int i = 1 ; i < n ; i++) {
-            if(n%i == 0) {
-                // A player can play
-                ans &= func(n-i,(turn == 1) ? 2 : 1);
+    bool divisorGame(int n) {
+        vector<bool> dp(n+1);
+        dp[1] = false;
+
+        for(int i = 2 ; i <= n ; i++) {
+            for(int j = i-1 ; j > 0 ; j--) {
+                
+                if(i%j == 0) {
+                    int val = i-j;
+                    dp[i] = dp[i] | !dp[val];
+                }
             }
         }
-        return ans;
-    }
-    bool divisorGame(int n) {
-        // 0 < x < n && n % x == 0
-        // replace n -> n-x
-        // player loses if they cannot make a move
-        // return true if alice wins else false
-        // both are playing optimally
-
-        // return func(n,1);
-        // return 1;
-        if(n&1) return 0;
-        else return 1;
+        return dp[n];
     }
 };
