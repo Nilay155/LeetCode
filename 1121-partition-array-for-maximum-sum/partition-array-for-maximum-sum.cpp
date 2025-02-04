914cec5757038949542fcffa727201494cd7d7ca
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-02-04 23:36:05 +0530

    Time: 6 ms (56.50%) | Memory: 12.5 MB (31.23%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -24,17 +24,17 @@ public:
         int n = arr.size();
         // return solve(arr,k,0,n);
         vector<int> dp(n+1,0);
-        for(int i = n-1 ; i >= 0 ; i--) {
+        for(int i = 1 ; i <= n ; i++) {
             int temp = 0;
             int maxi = -1e9;
-            for(int j = i ; j < n ; j++) {
-                if(j-i+1 <= k) {
-                    maxi = max(maxi,arr[j]);
-                    temp = max(temp,maxi*(j-i+1) + dp[j+1]);
+            for(int j = i ; j >= 1 ; j--) {
+                if(i-j+1 <= k) {
+                    maxi = max(maxi,arr[j-1]);
+                    temp = max(temp,maxi*(i-j+1) + dp[j-1]);
                 }
             }
             dp[i] = temp;
         }
-        return dp[0];
+        return dp[n];
     }
 };
