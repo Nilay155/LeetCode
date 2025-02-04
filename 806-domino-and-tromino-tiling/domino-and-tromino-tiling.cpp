6477758a0ab830ad55f1ebe5c96cfa287b5818ef
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-02-04 19:03:53 +0530

    Time: 11 ms (10.32%) | Memory: 9.1 MB (37.31%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,25 +1,21 @@
-#define ll long long
 class Solution {
 public:
-    ll MOD = 1e9 + 7;
-    ll solve(int n,vector<ll> &dp) {
-        if(n == 0) return 1;
-        if(n == 1) return 1;
-        if(n == 2) return 2; 
-
-        if(dp[n] != -1) return dp[n];
-
-        ll domino = (solve(n-1,dp) + solve(n-2,dp))%MOD;
-        ll tromino = 0;
-        if(n >= 3) {
-            for(ll i = 3 ; i <= n ; i++) {
-                tromino += (2*solve(n-i,dp))%MOD;
-            } 
-        }
-        return dp[n] = (((tromino + domino)%MOD))%MOD;
-    }
+    int MOD = 1e9 + 7;
     int numTilings(int n) {
-        vector<ll> dp(n+1,-1);
-        return solve(n,dp);
+       vector<long long int> dp(n+3);
+       dp[0] = 1;
+       dp[1] = 1;
+       dp[2] = 2;
+       dp[3] = 5;
+
+       for(int i = 4 ; i <= n ; i++) {
+            long long int domino = (dp[i-1] + dp[i-2])%MOD;
+            long long int tromino = 0;
+            for(int j = 3 ; j <= i ; j++) {
+                tromino = ((tromino + 2*dp[i-j])%MOD);
+            }
+            dp[i] = (domino + tromino) % MOD;
+       }  
+       return dp[n];
     }
 };
