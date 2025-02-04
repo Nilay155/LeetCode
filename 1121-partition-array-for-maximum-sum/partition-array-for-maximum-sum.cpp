18785b453fc0a0d6a77c1740d4a8240126121028
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-02-04 23:32:25 +0530

    Time: 171 ms (6.24%) | Memory: 12.3 MB (86.51%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxVal(vector<int> &arr,int i,int j) {
+        int maxi = -1e9;
+        for(int k = i ; k <= j ; k++) {
+            maxi = max(maxi,arr[k]);
+        }
+        return maxi;
+    }
+    int solve(vector<int> &arr,int &k,int i,int &n) {
+        if(i >= n) return 0;
+
+        int ans = 0;
+
+        for(int j = i ; j < n ; j++) {
+            if(j-i+1 <= k) {
+                // Valid Partition
+                ans = max(ans,maxVal(arr,i,j)*(j-i+1) + solve(arr,k,j+1,n));
+            }
+        }
+        return ans;
+    }
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        // return solve(arr,k,0,n);
+        vector<int> dp(n+1,0);
+        for(int i = n-1 ; i >= 0 ; i--) {
+            int temp = 0;
+            for(int j = i ; j < n ; j++) {
+                if(j-i+1 <= k) {
+                    temp = max(temp,maxVal(arr,i,j)*(j-i+1) + dp[j+1]);
+                }
+            }
+            dp[i] = temp;
+        }
+        return dp[0];
+    }
+};
