7713af87ee0cd31d5a6e8da8f283a67d44435216
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-02-08 00:40:50 +0530

    Time: 946 ms (15.33%) | Memory: 34.4 MB (17.58%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,53 +1,32 @@
 class Solution {
 public:
-    bool isValid(string &word,string &nextWord,unordered_map<string,bool> &visited){
-        
-        if(visited[nextWord]) return false ;
-        int n = word.size() ;
-
-        int count = 0 ;
-        for(int i  = 0 ; i < n ; i++){
-            if(word[i] != nextWord[i]) count++ ;
-        }
-        if(count == 1) return true ;
-        else return false  ;
-    }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        
-        int mini = INT_MAX;
-        int n = wordList.size() ;
-
-        bool flag = false ;
-        for(auto& it : wordList){
-            if(it == endWord) flag = true ;
-        }
-        if(!flag) return 0 ;
-        
-        unordered_map<string,bool> visited ;
-        queue<pair<string,int>> q ;
-
-        q.push({beginWord,1}) ;
-        visited[beginWord] = true ;
-
-        while(!q.empty()){
-            pair<string,int> p = q.front() ;
-            q.pop() ;
-
-            string word = p.first ;
-            int len = p.second ;
-
-            if(word == endWord){
-                mini = min(mini,len) ;
-            }
-
-            for(int i = 0 ; i < n ; i++){
-                if(isValid(word,wordList[i],visited)){
-                    q.push({wordList[i],len+1}) ;
-                    visited[wordList[i]] = true ;
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        queue<string> todo;
+        todo.push(beginWord);
+        int ladder = 1;
+        while (!todo.empty()) {
+            int n = todo.size();
+            for (int i = 0; i < n; i++) {
+                string word = todo.front();
+                todo.pop();
+                if (word == endWord) {
+                    return ladder;
+                }
+                dict.erase(word);
+                for (int j = 0; j < word.size(); j++) {
+                    char c = word[j];
+                    for (int k = 0; k < 26; k++) {
+                        word[j] = 'a' + k;
+                        if (dict.find(word) != dict.end()) {
+                            todo.push(word);
+                        }
+                     }
+                    word[j] = c;
                 }
             }
+            ladder++;
         }
-        if(mini == INT_MAX) return 0 ;
-        return mini ;
+        return 0;
     }
 };
