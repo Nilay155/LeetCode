e3cd3d963726f30829335f7ba28e3c13ae0a8640
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-02-09 15:14:59 +0530

    Time: 86 ms (17.02%) | Memory: 87.8 MB (52.89%) - LeetSync

---
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        int n = nums.size();
+        // j - i != nums[j] - nums[i] && i < j
+        // Brute Force -> O(n^2)
+        // Optimal Soln -> ?
+
+        unordered_map<int,int> mpp;
+
+        for(int j = 0 ; j < n ; j++) {
+            mpp[nums[j]-j]++;
+        }
+
+        long long ans = 0;
+
+        for(int i = 0 ; i < n ; i++) {
+            int val = nums[i]-i;
+            if(mpp.count(val)) {
+                ans += n-mpp[val]-i;
+            } else {
+                ans += n-i-1;
+            }
+            // cout << ans << endl;
+            mpp[val]--;
+            if(mpp[val] == 0) mpp.erase(val);
+        }
+        return ans;
+    }
+};
