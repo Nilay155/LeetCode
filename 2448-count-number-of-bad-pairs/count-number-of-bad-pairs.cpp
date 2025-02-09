db0efae06ae768383eb38b6e7f52ee5374106506
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-02-09 15:16:44 +0530

    Time: 104 ms (15.50%) | Memory: 88.3 MB (46.35%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    Solution() {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+        std::cout.tie(NULL);
+    }
     long long countBadPairs(vector<int>& nums) {
         int n = nums.size();
         // j - i != nums[j] - nums[i] && i < j
