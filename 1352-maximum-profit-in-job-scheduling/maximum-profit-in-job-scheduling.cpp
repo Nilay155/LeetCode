55a1e0cdb44072b5a4e759d110780192f4340d2b
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-02-11 23:18:46 +0530

    Time: 1037 ms (8.92%) | Memory: 108.6 MB (10.59%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    static bool cmp(vector<int> &a,vector<int> &b) {
+        if(a[1] < b[1]) return true;
+        else return false;
+    }
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = profit.size();
+        vector<vector<int>> nums;
+        for(int i = 0 ; i < n ; i++) {
+            nums.push_back({startTime[i],endTime[i],profit[i]});
+        }
+        sort(nums.begin(),nums.end(),cmp);
+
+        map<int,int> mpp;
+        mpp[0] = 0;
+
+        for(int i = 0 ; i < n ; i++) {
+            int start = nums[i][0];
+            int end = nums[i][1];
+            int value = nums[i][2];
+            
+            // cout << start << " " << end << endl;
+            auto itr = mpp.upper_bound(start);
+            
+            if (itr != mpp.begin()) {
+                --itr;  // Move to the largest key ≤ start
+            } else {
+                itr = mpp.end();  // No key ≤ start exists
+            }
+            mpp[end] = max(mpp[end],value + itr->second);
+            cout << itr -> first << " " << start << endl;
+
+            itr = mpp.lower_bound(end);
+            if(itr != mpp.begin()) {
+                --itr;
+            } else {
+                itr = mpp.end();
+            }
+            mpp[end] = max(mpp[end],mpp[itr->first]);
+
+        }
+
+        int ans = 0;
+        for(auto& it : mpp) {
+            ans = max(ans,it.second);
+        }
+        return ans;
+    }
+};
