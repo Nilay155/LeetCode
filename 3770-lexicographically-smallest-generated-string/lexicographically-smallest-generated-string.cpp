1ecda5474f9e1ef186d764aca72ddebc1a5ce9ae
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-03-11 01:15:51 +0530

    Time: 363 ms (6.08%) | Memory: 396.1 MB (5.08%) - LeetSync

---
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+
+public:
+    string generateString(string str1, string str2) {
+        int n = str1.length();
+        int m = str2.length();
+        string ans(n + m - 1, ' ');  
+        vector<int> blocked(n + m - 1, 0);
+
+        for(int i = 0 ; i < n ; i++) {
+            if(str1[i] == 'T') {
+                for(int j = 0; j < m; j++) {
+                    ans[i + j] = str2[j];  
+                    blocked[i + j] = 1;  
+                }
+            }
+        }
+
+        // Validate 'T' conditions
+        for(int i = 0; i < n; i++) {
+            if(str1[i] == 'T' && ans.substr(i, m) != str2) return "";
+            if(str1[i] == 'F' && ans.substr(i, m) == str2) return "";
+        }
+
+        // Filling 'F' positions
+        for(int i = 0; i < n; i++) {
+            if(str1[i] == 'F') {
+                for(int k = m-1 ; k >= 0 ; k--) {
+                    if(!blocked[i+k]) {
+                        for(char ch = 'a'; ch <= 'z'; ch++) {
+                            ans[i+k] = ch;
+                            if(ans.substr(i,m) != str2) break;
+                            else {
+                                cout << i << endl;
+                                blocked[i+k] = 1;
+                                ans[i+k] = ch + 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
