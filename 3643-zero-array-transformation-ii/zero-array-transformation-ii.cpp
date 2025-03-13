5ff9903a413315470d5f957f89f9ac4ad217c78f
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-03-14 00:47:41 +0530

    Time: 6 ms (85.79%) | Memory: 323 MB (74.49%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,52 +1,32 @@
 class Solution {
-private:
-    bool func(vector<int> &nums,vector<vector<int>>& queries,int start,int end) {
-        int n = nums.size();
-        vector<int> temp(n,0);
-        for(int i = start ; i <= end ; i++) {
-            int l = queries[i][0];
-            int r = queries[i][1];
-            int val = queries[i][2];
-
-            if(l >= n || l < 0) continue;
-
-            if(r+1 < n) temp[r+1] += val;
-            temp[l] -= val;
-        }
-        for(int i = 1;  i < n ; i++) temp[i] = temp[i] + temp[i-1];
-        for(int i = 0 ; i < n ; i++) {
-            if(nums[i] + temp[i] > 0)  {
-                return false;
-            }
-        }
-        return true;
-    }
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int m = queries.size();
-        int n = nums.size();
-        
-        int low = 0, high = m-1;
-        int k = INT_MAX;
-        int flag = false;
-        for(int i = 0 ; i < n ; i++) {
-            if(nums[i] > 0) {
-                flag = true;
-                break;
-            }
-        }
-        if(!flag) return 0;
-        
-        while(low <= high) {
-            int mid = (low + high) >> 1;
+        int n = nums.size(), sum = 0, k = 0;
+        vector<int> differenceArray(n + 1);
 
-            if(func(nums,queries,0,mid)) {
-                high = mid-1;
-                k = min(k,mid+1);
-            } else {
-                low = mid+1;
+        // Iterate through nums
+        for (int index = 0; index < n; index++) {
+            // Iterate through queries while current index of nums cannot equal
+            // zero
+            while (sum + differenceArray[index] < nums[index]) {
+                k++;
+
+                // Zero array isn't formed after all queries are processed
+                if (k > queries.size()) {
+                    return -1;
+                }
+                int left = queries[k - 1][0], right = queries[k - 1][1],
+                    val = queries[k - 1][2];
+
+                // Process start and end of range
+                if (right >= index) {
+                    differenceArray[max(left,index)] += val;
+                    differenceArray[right + 1] -= val;
+                }
             }
+            // Update prefix sum at current index
+            sum += differenceArray[index];
         }
-        return k == INT_MAX ? -1 : k;
+        return k;
     }
 };
