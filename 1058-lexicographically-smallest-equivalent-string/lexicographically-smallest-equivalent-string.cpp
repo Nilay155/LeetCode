15d2ce38102029852db3348853c1847dc44223af
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-06-05 19:34:35 +0530

    Time: 3 ms (41.19%) | Memory: 9.6 MB (29.50%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 private:
-    void dfs(int src,unordered_map<char,vector<char>> &graph,int &mini,vector<bool> &vis) {
+    void dfs(int src,unordered_map<char,vector<char>> &graph,vector<bool> &vis,vector<int> &minChar,int &mini) {
         vis[src] = true;
+        minChar[src] = mini;
 
         for(char child : graph[('a' + src)]) {
             if(!vis[(child-'a')]) {
-                mini = min(mini,child-'a');
-                dfs(child-'a',graph,mini,vis);
+                dfs(child-'a',graph,vis,minChar,mini);
             }
         }
     }
@@ -20,12 +20,18 @@ public:
             mpp[s1[i]].push_back(s2[i]); 
             mpp[s2[i]].push_back(s1[i]);
         }
+
+        vector<bool> vis(26,false);
+        vector<int> minChar(26,26);
+        for(int temp = 0 ; temp < 26 ; temp++) {
+            if(!vis[temp]) {
+                dfs(temp,mpp,vis,minChar,temp);
+            }
+        }
         string ans;
         for(int i = 0 ; i < m ; i++) {
-            vector<bool> vis(26,false);
-            int mini = baseStr[i]-'a';
-            dfs(mini,mpp,mini,vis);
-            char ch = 'a' + mini;  // ch = 'e' (because 'a' + 4 = 'e')
+            int mini = minChar[baseStr[i]-'a'];
+            char ch = 'a' + mini;
             ans.push_back(ch);
         }
         return ans;
