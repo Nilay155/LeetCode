fbb83488ed203acd8bce79666b8f2c74c7cc15e2
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-06-22 11:42:33 +0530

    Time: 5 ms (5.25%) | Memory: 13.4 MB (64.92%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,23 +1,36 @@
 class Solution {
 private:
-    int func1(vector<int> &nums) {
-        unordered_map<int,int> mpp;
-        for(int num : nums) mpp[num]++;
-        for(auto& it : mpp) {
-            if(it.second == 1) return it.first;
-        }
-        return -1;
-    }
-    // int func2(vector<int> &nums) {
+    // int func1(vector<int> &nums) {
+    //     unordered_map<int,int> mpp;
+    //     for(int num : nums) mpp[num]++;
+    //     for(auto& it : mpp) {
+    //         if(it.second == 1) return it.first;
+    //     }
+    //     return -1;
     // }
+    int func2(vector<int> &nums) {
+        int ans = 0;
+        for(int bitIndex = 0 ; bitIndex < 32 ; bitIndex++) {
+            int count = 0;
+            for(int num : nums) {
+                if(num & (1 << bitIndex)) {
+                    count += 1;
+                }
+            }
+            if(count%3 == 1) {
+                ans = ans | (1 << bitIndex);
+            }
+        }
+        return ans;
+    }
     // int func3(vector<int> &nums) {
     // }
     // int func4(vector<int> &nums) {
     // }
 public:
     int singleNumber(vector<int>& nums) {
-        return func1(nums);
-        // return func2(nums);
+        // return func1(nums);
+        return func2(nums);
         // return func3(nums);
         // return func4(nums);
     }
