a6926a1d2d4318c75ca4383bf25a2a52bf35e74c
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-09-09 10:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (5.18%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+private:
+    vector<int> computeLPS(string str) {
+        int prev = 0, curr = 1, n = str.length();
+        vector<int> lps(n,0);
+
+        while(curr < n) {
+            if(str[curr] == str[prev]) {
+                lps[curr] = 1 + prev;
+                curr += 1;
+                prev += 1;
+            } else if(prev == 0) {
+                prev = 0;
+                lps[curr] = 0;
+                curr += 1;
+            } else {
+                prev = lps[prev - 1];
+            }
+        }
+        return lps;
+    }
+    int kmp(string text,string patt) {
+        vector<int> lps = computeLPS(patt);
+        int t = 0, p = 0, n = text.length(), m = patt.length();
+
+        while(t < n) {
+            if(text[t] == patt[p]) {
+                t += 1;
+                p += 1;
+            } else if(p == 0) {
+                t += 1;
+            } else {
+                p = lps[p-1];
+            }
+
+            if(p >= m) return t-m;
+        }
+        return -1;
+    }
+    int rabinKarp(string text,string patt) {
+        return -1;
+    }
+public:
+    int strStr(string haystack, string needle) {
+        return kmp(haystack,needle);
+    }
+};
