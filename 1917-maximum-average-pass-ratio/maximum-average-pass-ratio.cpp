d73b4f11522fa4ea576e8b750d32ec0fb032a596
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-09-13 00:30:40 +0530

    Time: 275 ms (76.95%) | Memory: 98 MB (43.09%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -1,47 +1,33 @@
 class Solution {
+private:
+    double classRatio(int pass,int total) {
+        return double(pass+1)/double(total+1) - double(pass)/double(total);
+    }
 public:
-    static bool cmp(vector<int> &a,vector<int> &b) {
-
-        double k1 = a[0]/a[1];
-        double k2 = b[0]/b[1];
-
-        return k1 < k2;
-    }
-    double increment(int pass,int total) {
-
-        double res = (double(pass+1)/double(total+1)) - (double(pass)/double(total));
-        return res;
-    }
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
         int n = classes.size();
-        double ans = 0;
 
         priority_queue<pair<double,int>> pq;
+
+
         for(int i = 0 ; i < n ; i++) {
-            int pass = classes[i][0];
-            int total = classes[i][1];
-            pq.push({increment(pass,total),i});
+            double ratio = classRatio(classes[i][0],classes[i][1]);
+            pq.push({ratio,i});
         }
 
-        for(int i = 0 ; i < extraStudents; i++) {
-            
-            auto p = pq.top();
-            pq.pop();
-            
-
-            int k = p.second;
-            
-            classes[k][0] = classes[k][0] + 1;
-            classes[k][1] = classes[k][1] + 1;
-            pq.push({increment(classes[k][0],classes[k][1]),k});
+        while(extraStudents--) {
+            auto [ratio,i] = pq.top(); pq.pop();
+            classes[i][0] += 1;
+            classes[i][1] += 1;
+            double newRatio = classRatio(classes[i][0],classes[i][1]);
+            pq.push({newRatio,i});
         }
 
-        while(!pq.empty()) {
-            auto p = pq.top();
-            pq.pop();
-            int k = p.second;
-            ans += double(classes[k][0])/double(classes[k][1]);
+        double ans = 0.0;
+        for(int i = 0 ; i < n ; i++) {
+            double ratio = double(classes[i][0])/double(classes[i][1]);
+            ans += ratio;
         }
-        return (ans/n);
+        return double(ans) / double(n);
     }
 };
