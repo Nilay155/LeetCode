d9d7ce117c6e5d3d5be41dba58fb634de50c7ab8
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-09-17 15:18:42 +0530

    Time: 539 ms (5.01%) | Memory: 30.5 MB (48.39%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -37,8 +37,7 @@ public:
 
         for(int i = 1 ; i <= n ; i++) {
             for(int j = 1 ; j <= m ; j++) {
-                if(s[i-1] == p[j-1]) dp[i][j] = dp[i-1][j-1];
-                else if(p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                if(s[i-1] == p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
                 else if(p[j-1] == '*') dp[i][j] = dp[i-1][j] | dp[i][j-1];
                 else dp[i][j] = false;
             }
