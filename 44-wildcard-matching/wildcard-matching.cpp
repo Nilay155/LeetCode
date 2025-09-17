3a60c4a013898043b5a553583c265dbaa322e7c3
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-09-17 15:18:19 +0530

    Time: 505 ms (5.01%) | Memory: 30.6 MB (48.39%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,87 +1,48 @@
 class Solution {
-public:
-    bool check(string &s,int j,int m) {
-
-        for(int k = j ; k < m ; k++) {
-            if(s[k] != '*') return false;
+private:
+    int dp[2001][2001];
+    int f(string &s,string &p,int i,int j) {
+        // Base-Cases
+        if(i == 0 && j == 0) return true;
+        if(i == 0 && j != 0) {
+            while(j > 0) {
+                if(p[j-1] != '*') return false;
+                j--;
+            }
+            return true;
         }
-        return true;
-    }
-    bool dfs(string &s1,string &s2,int i,int j,int &n,int &m,vector<vector<int>> &dp) {
-
-        if(i >= n && j >= m) return true;
-        if(i >= n) {
-            if(check(s2,j,m)) return true;
-            else return false;
-        }
-        if(j >= m) return false;
+        if(i == 0 || j == 0) return false;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int option1 = false;
-        int option2 = false;
-        int option3 = false;
-
-        if(s1[i] == s2[j]) {
-            option1 = dfs(s1,s2,i+1,j+1,n,m,dp);
-        } else if(s2[j] == '?') {
-            option2 = dfs(s1,s2,i+1,j+1,n,m,dp);
-        } else if(s2[j] == '*') {
-            option3 = dfs(s1,s2,i+1,j,n,m,dp) || dfs(s1,s2,i,j+1,n,m,dp);
-        } else { 
-            return false;
-        }
-
-        return dp[i][j] =  option1 || option2 || option3;
+        if(s[i-1] == p[j-1]) return dp[i][j] = f(s,p,i-1,j-1);
+        else if(p[j-1] == '?') return dp[i][j] = f(s,p,i-1,j-1);
+        else if(p[j-1] == '*') return dp[i][j] = f(s,p,i-1,j) || f(s,p,i,j-1);
+        else return dp[i][j] = false;
     }
+public:
+    bool isMatch(string s, string p) {
+        // memset(dp,-1,sizeof(dp));
+        int n = s.length(), m = p.length();
+        // return f(s,p,n,m);
 
-    bool isPossible(string s,int i,int m) {
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        for(int j = 0 ; j <= m ; j++)
+            dp[0][j] = true;
 
-        for(int j = i ; j < m ; j++) {
-            if(s[j] != '*') return false;
-        }
-        return true;
-    }
-    bool tabulation(string s,string p) {
-        int n = s.length();
-        int m = p.length();
-
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-
-        for(int i = 0 ; i < m ; i++) {
-            dp[n][i] = isPossible(p,i,m);
+        for(int j = 1 ; j <= m ; j++) {
+            if(p[j-1] == '*') dp[0][j] = dp[0][j] & dp[0][j-1];
+            else dp[0][j] = false;
         }
 
-        for(int i = 0 ; i < n ; i++) {
-            dp[i][m] = false;
-        }
-        dp[n][m] = true;
-
-        for(int i = n-1 ; i >= 0 ; i--) {
-            for(int j = m-1 ; j >= 0 ; j--) {
-
-                if(s[i] == p[j]) {
-                    dp[i][j] = dp[i+1][j+1];
-                } else if(p[j] == '?') {
-                    dp[i][j] = dp[i+1][j+1];
-                } else if(p[j] == '*') {
-                    dp[i][j] = dp[i+1][j] || dp[i][j+1];
-                } else {
-                    dp[i][j] = false;
-                }
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= m ; j++) {
+                if(s[i-1] == p[j-1]) dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*') dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                else dp[i][j] = false;
             }
         }
-        return dp[0][0];
-
-    }
-    bool isMatch(string s, string p) {
-        
-        int n = s.length();
-        // int m = p.length();
-        
-        // int i = 0, j = 0;
-        // vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        // return dfs(s,p,i,j,n,m,dp);
-        return tabulation(s,p);
+        return dp[n][m];
     }
 };
