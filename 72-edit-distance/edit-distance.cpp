6c98fd34eeed43115f155dec5a6e265f40d507a2
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-09-25 15:47:38 +0530

    Time: 49 ms (16.05%) | Memory: 11.7 MB (85.75%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,28 +1,29 @@
 class Solution {
+private:
+    int dp[501][501];
+    int f(string &s,string &t,int i,int j) {
+        if(i == 0) {
+            return j;
+        }
+        if(j == 0) {
+            return i;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i-1] == t[j-1]) return dp[i][j] = f(s,t,i-1,j-1);
+        else {
+            // replace
+            int replace = 1 + f(s,t,i-1,j-1);
+            int deletee = 1 + f(s,t,i-1,j);
+            int insert = 1 + f(s,t,i,j-1);
+
+            return dp[i][j] =  min({replace,insert,deletee});
+        }
+    }
 public:
-    int minDistance(string s1, string s2) {
-        int n = s1.length();
-        int m = s2.length();
-
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-
-        for(int i = n ; i >= 0 ; i--) {
-            dp[i][m] = n-i;
-        }
-        for(int i = m ; i >= 0 ; i--) {
-            dp[n][i] = m-i;
-        }
-        for(int i = n-1 ; i >= 0 ; i--) {
-
-            for(int j = m-1 ; j >= 0 ; j--) {
-
-                if(s1[i] == s2[j]) {
-                    dp[i][j] = dp[i+1][j+1];
-                } else {
-                    dp[i][j] = 1 + min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
-                }
-            }
-        }
-        return dp[0][0];
+    int minDistance(string word1, string word2) {
+        int n = word1.length(), m = word2.length();
+        memset(dp,-1,sizeof(dp));
+        return f(word1,word2,n,m);
     }
 };
