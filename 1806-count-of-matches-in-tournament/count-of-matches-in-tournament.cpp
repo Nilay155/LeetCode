aa16fa77ba1bd0f88b0221a6a066369e31cca607
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-12-26 00:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.70%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        
+        int totalMatches = 0, ref = n;
+        while(n > 1) {
+            totalMatches += n/2;
+            if(n&1) n = n/2 + 1;
+            else n /= 2;
+        }
+        return totalMatches;
+    }
+};
