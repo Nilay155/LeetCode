b16483fdb689384f9e89eb6820ca5d95848f9c39
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2025-12-27 16:21:05 +0530

    Time: 7 ms (27.91%) | Memory: 23.5 MB (13.77%) - LeetSync

--- 3311-ant-on-the-boundary/ant-on-the-boundary.cpp
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.cpp
@@ -5,9 +5,7 @@ public:
         int ans = 0;
         int move = 0;
         for(int i = 0 ; i < n ; i++) {
-            if(nums[i] < 0) move += nums[i];
-            else move += nums[i];
-
+            move += nums[i];
             if(move == 0) ans += 1;
         }
         return ans;
