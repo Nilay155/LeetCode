a6a6e49a37997bc6dfdcf1dbe8db8fbdac284bd8
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-08 23:51:26 +0530

    Time: 111 ms (8.96%) | Memory: 199.9 MB (5.37%) - LeetSync

--- 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -7,13 +7,15 @@ public:
         events.push_back({0,0});
         for(int i = 0 ; i < n ; i++) events.push_back({st[i],et[i]});
         events.push_back({tt,tt});
-
-        map<int,int> gaps;
-        for(int i = 0 ; i < events.size() - 1 ; i++) {
-            gaps[events[i+1][0] - events[i][1]] += 1;
+        
+        vector<int> p(events.size()), s(events.size());
+        for(int i = 1 ; i < events.size() - 1 ; i++) {
+            p[i] = max(events[i][0] - events[i-1][1],p[i-1]);
+        }
+        for(int i = events.size()-2 ; i >= 1 ; i--) {
+            s[i] = max(events[i+1][0] - events[i][1],s[i+1]);
         }
         // for(auto[g,c] : gaps) cout << g << "  " << c << "\n";
-        gaps.erase(0);
 
         int ans = 0;
         for(int i = 1 ; i < events.size()-1 ; i++) {
@@ -26,18 +28,8 @@ public:
             ans = max(ans,rs - (le + (ce - cs)));
 
             // place somewhere between other blocks
-            int lg = cs - le, rg = rs - ce;
-            auto it = gaps.lower_bound(ce-cs);
-            int g = it -> first, c = it -> second;
-            while(it != gaps.end() && !((g == lg && g == rg && c > 2) || (g == lg && g != rg && c > 1) || (g != lg && g == rg && c > 1) || (g != lg && g != rg && c > 0))) {
-                it++;
-                if(it != gaps.end()) {
-                    g = it -> first, c = it -> second;
-                }
-            }
-            if(it != gaps.end()) {
-                ans = max(ans,rs - le);
-            }
+            int suffix = s[i+1], prefix = p[i-1];
+            if(suffix >= ce-cs || prefix >= ce-cs) ans = max(ans,rs - le);
             // taking the default time
             ans = max({ans,cs - le,rs - ce});
         }
