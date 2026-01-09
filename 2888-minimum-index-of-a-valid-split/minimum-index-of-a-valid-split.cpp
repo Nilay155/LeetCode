6f98d019161f917c63ea49a8d58bac5ad370e476
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-09 15:45:57 +0530

    Time: 29 ms (60.92%) | Memory: 98.4 MB (66.15%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> count;
+        unordered_map<int,int> counts;
+        for(int num : nums) counts[num] += 1;
 
-        for(int num : nums) count[num]++;
-
-        int dominantElement = -1;
-        for(auto& it : count) {
-            if(it.second > n/2) {
-                dominantElement = it.first;
-                break;
+        int val = -1, maxi = 0;
+        for(auto [v,c] : counts) {
+            if(c > maxi) {
+                maxi = c, val = v;
             }
         }
 
-        int minIndex = -1;
-        int cnt = 0;
-        int total = count[dominantElement];
+        int prefixCount = 0;
+        for(int i = 0 ; i <= n-2 ; i++) {
+            prefixCount += (nums[i] == val ? 1 : 0);
 
-        for(int i = 0 ; i < n ; i++) {
-            if(nums[i] == dominantElement) cnt += 1;
-
-            if((i+1)/2 < cnt && (n-i-1)/2 < total-cnt) return i;
+            int left = prefixCount, right = maxi - left;
+            if(left > (i+1) / 2 && right > (n - i - 1) / 2) 
+                return i;
         }
-        return minIndex;
+        return -1;
     }
 };
