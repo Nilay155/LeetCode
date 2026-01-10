dbb42eaacacf2ee90ddbf9bd7d0b8ef50b78269c
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-10 13:59:08 +0530

    Time: 8 ms (86.04%) | Memory: 38.1 MB (38.11%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -3,19 +3,22 @@ public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size();
         int ans = 0;
-        for(int i = 0 ; i < n ; i++) { 
-            // row => 0 to n-1
-            int j = 0;
-            for(int c = 0 ; c < n ; c++) {
-                bool flag = true;
-                for(int r = 0 ; r < n ; r++, j++) {
-                    if(grid[r][c] != grid[i][j]) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag) ans += 1;
-                j = 0;
+        map<vector<int>,int> R,C;
+        for(int i = 0 ; i < n ; i++) {
+            R[grid[i]] += 1;
+        }
+        for(int i = 0 ; i < n ; i++) {
+            vector<int> temp;
+            for(int j = 0 ; j < n ; j++) {
+                temp.push_back(grid[j][i]);
+            }
+            C[temp] += 1;
+        }
+
+        for(auto [arr,count] : R) {
+
+            if(C.find(arr) != C.end()) {
+                ans += (count * C[arr]);
             }
         }
         return ans;
