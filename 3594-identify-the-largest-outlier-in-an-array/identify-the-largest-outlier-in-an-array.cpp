0844b8c47ac5a646d94fe579e385f7d079093f23
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-11 17:09:43 +0530

    Time: 191 ms (23.19%) | Memory: 193.6 MB (8.33%) - LeetSync

---
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int getLargestOutlier(vector<int>& nums) {
+        int n = nums.size();
+        // sort(nums.begin(),nums.end());
+        unordered_map<int,bool> map;
+        unordered_map<int,int> count;
+        
+        int sum = 0;
+        for(int x : nums) {
+            map[2*x] = true;
+            count[x] += 1;
+            sum += x;
+        }
+        int ans = -1001;
+        for(int i = 0 ; i < n ; i++) {
+            int rem = (sum - nums[i]);
+            if(map.find(rem) != map.end()) {
+                if(rem == 2*nums[i]) {
+                    if(count[nums[i]] > 1) 
+                        ans = max(ans,nums[i]);
+                } else {
+                    ans = max(ans,nums[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
