b73c530fb66f751ceb9ffd492923b4faf3477bc8
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-15 13:15:52 +0530

    Time: 14 ms (24.00%) | Memory: 11.8 MB (52.14%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
-    double solve(int m,int n,int k,int row,int col) {
-        if(row < 0 || col < 0 || row > m || col > n || k < 0) return 0;
-        if(k == 0) return 1;
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<int>> moves = {
+            {-1,2},{1,2},{-1,-2},{1,-2},{2,1},{2,-1},{-2,1},{-2,-1}
+        };
 
-        double op1 = 0.1250 * solve(m,n,k-1,row-2,col+1);
-        double op2 = 0.1250 * solve(m,n,k-1,row-1,col+2);
-        double op3 = 0.1250 * solve(m,n,k-1,row+1,col+2);
-        double op4 = 0.1250 * solve(m,n,k-1,row+2,col+1);
-        double op5 = 0.1250 * solve(m,n,k-1,row+2,col-1);
-        double op6 = 0.1250 * solve(m,n,k-1,row+1,col-2);
-        double op7 = 0.1250 * solve(m,n,k-1,row-1,col-2);
-        double op8 = 0.1250 * solve(m,n,k-1,row-2,col-1);
+        vector<vector<double>> board(n,vector<double>(n,0.0));
+        board[row][column] = 1.00;
 
-        double ans = op1+op2+op3+op4+op5+op6+op7+op8;
-        return ans;
-    }
-    bool isValid(int r, int c, int n) {
-        return r >= 0 && r < n && c >= 0 && c < n;
-    }
-    double tabulation(int m,int n,int k,int row,int col) {
-        vector<vector<double>> dp(m+1,vector<double>(n+1,0));
-        dp[row][col] = 1;
-        vector<vector<int>> moves = 
-        {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
-        for(int move = 1 ; move <= k ; move++) {
-            vector<vector<double>> next(m+1,vector<double>(n+1,0));
+        for(int m = 1 ; m <= k ; m++) {
+            vector<vector<double>> nextBoard(n,vector<double>(n,0.0));
+            for(int i = 0 ; i < n ; i++) {
+                for(int j = 0 ; j < n ; j++) {
+                    
+                    for(int k = 0 ; k < 8 ; k++) {
+                        int x = i + moves[k][0], y = j + moves[k][1];
 
-            for(int i = 0 ; i <= m ; i++) {
-                for(int j = 0 ; j <= n ; j++) {
-                    for (const auto& m : moves) {
-                        int newR = i + m[0];
-                        int newC = j + m[1];
-                        if (isValid(newR, newC, n+1)) {
-                            next[i][j] += dp[newR][newC] / 8.0;
+                        if(x >= 0 && x < n && y >= 0 && y < n) {
+                            nextBoard[x][y] += (0.125 * board[i][j]);
                         }
                     }
                 }
             }
-            dp = next;
+            // for(int i = 0 ; i < n ; i++) {
+            //     for(int j = 0 ; j < n ; j++) {
+            //         cout << nextBoard[i][j] << "  ";
+            //     }    cout << "\n";
+            // }
+            board = nextBoard;
         }
-        double probability = 0;
-        for (int r = 0; r <= n; r++) {
-            for (int c = 0; c <= n; c++) {
-                probability += dp[r][c];
-                // cout << dp[r][c] << "  ";
-            }cout << endl;
+        double ans = 0;
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                ans += board[i][j];
+            }
         }
-
-        return probability;
-    }
-    double knightProbability(int n, int k, int row, int column) {
-        return tabulation(n-1,n-1,k,row,column);
+        return ans;
     }
 };
