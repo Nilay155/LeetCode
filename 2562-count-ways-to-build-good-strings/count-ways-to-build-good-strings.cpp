ffeda4692e33ff67c985822fa133541488e6d60f
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-20 13:58:31 +0530

    Time: 4 ms (79.39%) | Memory: 11.7 MB (80.20%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,19 +1,22 @@
 class Solution {
+private:
+    int MOD = 1e9 + 7;
 public:
-    int MOD = 1e9+7;
-    int dp[100001];
-    int solve(int low,int high,int zero,int one,int len) {
-        if(len > high) return 0;
-        if(dp[len] != -1) return dp[len];
-
-        int ans = 0;
-        if(len >= low && len <= high) ans++;
-
-        ans = (ans + (solve(low,high,zero,one,len+zero) + solve(low,high,zero,one,len+one))%MOD)%MOD;
-        return dp[len] = ans; 
-    }
     int countGoodStrings(int low, int high, int zero, int one) {
-        memset(dp,-1,sizeof(dp));
-        return solve(low,high,zero,one,0);
+        
+        vector<int> dp(high+1,0);
+        dp[0] = 1;
+
+        for(int len = 1 ; len <= high ; len++) {
+
+            if(len - zero >= 0) dp[len] = (dp[len] + dp[len - zero]) % MOD;
+            if(len - one >= 0) dp[len] = (dp[len] + dp[len - one]) % MOD;
+        }
+        int ans = 0;
+
+        for(int k = low ; k <= high ; k++) {
+            ans = (ans + dp[k]) % MOD;
+        }
+        return ans;
     }
 };
