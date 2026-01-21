49e354001cefd1f2eb91eacdb7d9a64814553fd4
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-21 16:09:59 +0530

    Time: 96 ms (19.54%) | Memory: 89.9 MB (24.14%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> counts;
-        for(int num : nums) counts[num] += 1;
+        sort(nums.rbegin(),nums.rend());
 
-        vector<vector<int>> arr;
-        for(auto [num,count] : counts) arr.push_back({num,count});
-        sort(arr.rbegin(),arr.rend());
+        int prefix = 1, ans = 0;
+        for(int i = 1 ; i < n ; i++) {
 
-        int prefix = arr[0][1], ans = 0;
-        for(int i = 1 ; i < (int)arr.size() ; i++) {
-            ans += prefix;
-            prefix += arr[i][1];
+            if(nums[i] != nums[i-1]) {
+                ans += prefix;
+            }
+            prefix += 1;
         }
         return ans;
     }
