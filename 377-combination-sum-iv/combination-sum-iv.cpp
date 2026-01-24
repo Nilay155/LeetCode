cf1dfb85c9abf478ec297e2082e20d62446f07ba
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-24 16:53:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (99.39%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -12,7 +12,20 @@ private:
         }
         return dp[target] = ans;
     }
-    int tabulation(vector<int> &nums,int &n,int &target);
+    int tabulation(vector<int> &nums,int &n,int &target) {
+
+        memset(dp,0,sizeof(dp));
+        dp[0] = 1;
+
+        for(int t = 1 ; t <= target ; t++) {
+
+            for(int j = 0 ; j < n ; j++) {
+                if(nums[j] <= t)
+                    dp[t] += dp[t - nums[j]];
+            }
+        }
+        return dp[target];
+    }
 public:
     int combinationSum4(vector<int>& nums, int target) {
         
