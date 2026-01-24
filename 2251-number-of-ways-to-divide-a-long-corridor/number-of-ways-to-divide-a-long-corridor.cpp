bc9e9612401604b026057e381813119d8b7fd6b2
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-24 16:23:52 +0530

    Time: 107 ms (8.07%) | Memory: 42.8 MB (48.25%) - LeetSync

---
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    int MOD = 1e9 + 7;
+    int dp[100000][3];
+   int ways(string &str, int i, int count, int &n) {
+    if (i >= n) return (count == 2);
+    if (dp[i][count] != -1) return dp[i][count];
+
+    int ans = 0;
+
+    if(str[i] == 'S') {
+        if(count == 2) {
+            ans = (ans + ways(str,i+1,1,n)) % MOD;
+        } else {
+            ans = (ans + ways(str,i+1,count+1,n)) % MOD;
+        }
+    } else {
+        if(count == 2) {
+            ans = (ans + ways(str,i+1,count,n)) % MOD;
+            ans = (ans + ways(str,i+1,0,n)) % MOD;
+        } else {
+            ans = (ans + ways(str,i+1,count,n)) % MOD;
+        }
+    }
+    return dp[i][count] = ans;
+}
+
+public:
+    int numberOfWays(string corridor) {
+        int n = corridor.length();
+        memset(dp,-1,sizeof(dp));
+        return ways(corridor,0,0,n);
+    }
+};
