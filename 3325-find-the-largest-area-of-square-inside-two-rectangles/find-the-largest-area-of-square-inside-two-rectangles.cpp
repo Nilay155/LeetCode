e3a4a3f5310aa930583f25b97af8c996c377026b
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-01-25 13:47:51 +0530

    Time: 117 ms (32.06%) | Memory: 59.3 MB (78.75%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -5,19 +5,18 @@ public:
         int n = bottomLeft.size();
         ll side = 0;
         for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ; j < n ; j++) {
-                if(i != j) {
-                    int minx = max(bottomLeft[i][0],bottomLeft[j][0]);
-                    int maxx = min(topRight[i][0],topRight[j][0]);
+            for(int j = i+1 ; j < n ; j++) {
+                
+                int minx = max(bottomLeft[i][0],bottomLeft[j][0]);
+                int maxx = min(topRight[i][0],topRight[j][0]);
 
-                    int miny = max(bottomLeft[i][1],bottomLeft[j][1]);
-                    int maxy = min(topRight[i][1],topRight[j][1]);
+                int miny = max(bottomLeft[i][1],bottomLeft[j][1]);
+                int maxy = min(topRight[i][1],topRight[j][1]);
 
-                    if(miny > maxy || minx > maxx) continue;
-                    ll length = maxx - minx;
-                    ll breadth = maxy - miny;
-                    side = max(side,min(length,breadth));
-                }
+                if(miny > maxy || minx > maxx) continue;
+                ll length = maxx - minx;
+                ll breadth = maxy - miny;
+                side = max(side,min(length,breadth));
             }
         }
         return 1LL * side * side;
