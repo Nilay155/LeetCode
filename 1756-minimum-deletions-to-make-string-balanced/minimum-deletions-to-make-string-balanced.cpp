33104f359e9f6aca7f50492275cfed3acfd6b3a5
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-02-08 23:21:45 +0530

    Time: 32 ms (43.82%) | Memory: 25.6 MB (72.94%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,25 +1,48 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        // we need the string in the form of (aaaa..bbbb.. or aaaa.. or bbbb..) 
-        // but not in the form (aa..bb..aa.. or bb..aa..bb..)
-
         int n = s.length();
-        stack<char> st;
-        int res = 0;
 
-        for(int i = 0 ; i < n ; i++) {
-            if(i == 0) {
-                st.push(s[i]);
+        int a = 0, b = 0;
+        for(char ch : s) {
+            if(ch == 'a') a += 1;
+            else b += 1;
+        }
+        int l = 0, r = n-1;
+        // while(r >= 0 && s[r] == 'b') r -= 1;
+        // while(l < n && s[l] == 'a') l += 1;
+
+        int ans = 1e5,c = 0, x = 0;
+        char ch;
+
+        for(int i = r ; i >= l ; i--) {
+            if(c == 0) {
+                c = 1;
+                ch = s[i];
             } else {
-                if(!st.empty() && st.top() == 'b' && s[i] == 'a') {
-                    st.pop();
-                    res += 1;
-                } else {
-                    st.push(s[i]);
+
+                if(ch == s[i]) c += 1;
+                else {
+                    if(ch == 'a') {
+                        int op1 = b;
+                        int op2 = c;
+                        ans = min(ans,x + op1);
+                        x += op2;
+                    } else {
+                        b -= c;
+                    }
+                    c = 1, ch = s[i];
                 }
+                
             }
         }
-        return res;
+        if(ch == 'a') {
+            int op1 = b;
+            int op2 = c;
+            ans = min(ans,x + op1);
+            x += op2;
+        }
+        ans = min(ans,x);
+        return ans == 1e5 ? 0 : ans;
     }
 };
