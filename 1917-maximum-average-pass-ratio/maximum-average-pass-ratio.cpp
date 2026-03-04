ce14c391cc0de839ddb51c3ff144c0aab23826da
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-03-04 21:56:56 +0530

    Time: 280 ms (79.45%) | Memory: 97.7 MB (86.79%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -1,33 +1,32 @@
 class Solution {
 private:
-    double classRatio(int pass,int total) {
-        return double(pass+1)/double(total+1) - double(pass)/double(total);
+    double calculateGainRatio(int pass,int total) {
+        double oldRatio = (double)pass/(double)total;
+        double newRatio = (double)(pass + 1)/(double)(total + 1);
+
+        double gain = newRatio - oldRatio;
+        return gain;
     }
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
         int n = classes.size();
-
-        priority_queue<pair<double,int>> pq;
-
-
+        priority_queue<pair<double,int>> ratioGains;
         for(int i = 0 ; i < n ; i++) {
-            double ratio = classRatio(classes[i][0],classes[i][1]);
-            pq.push({ratio,i});
+            ratioGains.push({calculateGainRatio(classes[i][0],classes[i][1]) , i});
         }
 
         while(extraStudents--) {
-            auto [ratio,i] = pq.top(); pq.pop();
-            classes[i][0] += 1;
-            classes[i][1] += 1;
-            double newRatio = classRatio(classes[i][0],classes[i][1]);
-            pq.push({newRatio,i});
+            auto [gain,index] = ratioGains.top(); ratioGains.pop();
+            classes[index][0] += 1;
+            classes[index][1] += 1;
+            ratioGains.push({calculateGainRatio(classes[index][0],classes[index][1]) , index});
         }
 
-        double ans = 0.0;
+        double ans = 0.00;
         for(int i = 0 ; i < n ; i++) {
-            double ratio = double(classes[i][0])/double(classes[i][1]);
+            double ratio = (double) classes[i][0] / (double) classes[i][1];
             ans += ratio;
         }
-        return double(ans) / double(n);
+        return ans/(double)n;
     }
 };
