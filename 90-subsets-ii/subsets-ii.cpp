9529bd4b4dcbad3c4e760aa48249b463454ff2c3
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-03-28 00:42:25 +0530

    Time: 1 ms (44.68%) | Memory: 13.9 MB (12.24%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,29 +1,39 @@
 class Solution {
 private:
-    set<vector<int>> st;
     vector<vector<int>> ans;
     vector<int> arr;
 
-    void f(vector<int> &nums,int i,int n) {
-        if(i >= n) {
-            st.insert(arr);
+    void f(int i,vector<int> &freq,vector<int> &nums) {
+        if(i >= nums.size()) {
+            ans.push_back(arr);
             return ;
         }
 
-        f(nums,i + 1,n);
-
-        arr.push_back(nums[i]);
-        f(nums,i + 1,n);
-        arr.pop_back();
+        for(int c = 0 ; c <= freq[nums[i] + 10] ; c++) {
+            
+            if(c > 0) {
+                arr.push_back(nums[i]);
+                f(i + 1,freq,nums);
+            } else {
+                f(i + 1,freq,nums);
+            }
+        }
+        while(!arr.empty() && arr.back() == nums[i]) 
+            arr.pop_back();
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        f(nums,0,n);
-        for(auto it : st) {
-            ans.push_back(it);
+        vector<int> freq(21,0);
+        for(int f : nums) freq[f + 10] += 1;
+        nums.clear();
+        for(int i = 0 ; i <= 20 ; i++) {
+            if(freq[i])
+                nums.push_back(i - 10);
         }
+        f(0,freq,nums);
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
