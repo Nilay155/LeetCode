0ea67aedfbb2c118ef79f3ed0129aed27f403d3b
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-03-29 16:49:12 +0530

    Time: 4 ms (23.04%) | Memory: 112.5 MB (60.41%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    pair<int,int> findValidStartingIndex(vector<int> &gas,vector<int> &cost) {
+        int n = gas.size();
+
+        int l = 0, r = 1, sum = gas[0];
+
+        while(r < n) {
+            if(sum - cost[r - 1] >= 0) {
+                sum -= cost[r - 1];
+                sum += gas[r];
+            } else {
+                while(l < r && sum - cost[r - 1] < 0) {
+                    sum += cost[l];
+                    sum -= gas[l];
+                    l += 1;
+                }
+            }
+            if(l == r) sum = gas[r];
+            r += 1;
+        }
+        return {l,sum};
+    }
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+
+        auto [r,rSum] = findValidStartingIndex(gas,cost);
+        int lSum = -cost[n - 1], l = 0, mini = -cost[n - 1];
+        while(l < r) {
+            lSum += gas[l];
+            lSum -= cost[l];
+            mini = min(mini,lSum);
+            l += 1;
+        }
+
+        while(r < n) {
+            // cout << r << "  " << rSum << "  " << mini << "\n";
+            if(rSum >= abs(mini))
+                return r;
+            
+            rSum -= gas[r];
+            rSum += cost[r];
+            
+
+            lSum += gas[l];
+            lSum -= cost[l];
+            mini = min(mini,lSum);
+
+            l += 1;
+            r += 1;
+        }
+        return -1;
+    }
+};
