eb6535e72e04d8c43633e430635a0fccdb7093db
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-04-03 16:35:02 +0530

    Time: 367 ms (47.25%) | Memory: 185.8 MB (34.04%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,21 +1,44 @@
 class Solution {
+private:
+    void mergeArrays(vector<int> &nums,int l,int r,int mid) {
+        vector<int> left(mid - l + 1), right(r - mid);
+
+        for(int k = 0 ; k < mid - l + 1 ; k++) {
+            left[k] = nums[l + k];
+        }
+        for(int k = 0 ; k < r - mid ; k++) {
+            right[k] = nums[mid + k + 1];
+        }
+        int k = l;
+        int i = 0, n = left.size();
+        int j = 0, m = right.size();
+
+        while(i < n && j < m) {
+            if(left[i] >= right[j]) 
+                nums[k++] = right[j++];
+            else
+                nums[k++] = left[i++];
+        }
+        while(i < n) {
+            nums[k++] = left[i++];
+        }
+        while(j < m) {
+            nums[k++] = right[j++];
+        }
+        return ;
+    }
+    void mergeSort(vector<int> &nums,int l,int r) {
+        if(l >= r)
+            return ;
+        
+        int mid = l + (r - l) / 2;
+        mergeSort(nums,l,mid);
+        mergeSort(nums,mid + 1, r);
+        mergeArrays(nums,l,r,mid);
+    }
 public:
     vector<int> sortArray(vector<int>& nums) {
-        
-        priority_queue<int,vector<int>,greater<int>> pq;
-
-        for(int num : nums) {
-            pq.push(num);
-        }
-
-        vector<int> res;
-
-        while(!pq.empty()) {
-            int num = pq.top();
-            pq.pop();
-
-            res.push_back(num);
-        }
-        return res;
+        mergeSort(nums,0,nums.size() - 1);
+        return nums;
     }
 };
