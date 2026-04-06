6ff364cab82852e69fdc2fcfb3e62d068db639d3
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-04-07 01:52:31 +0530

    Time: 43 ms (91.25%) | Memory: 117.7 MB (91.06%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,48 +1,19 @@
 class Solution {
 public:
-    vector<bool> arr;
-    bool isPossible(vector<int> &banned,int n,int maxSum,int k) {
-
-        int sum = 0;
-        int count = 0;
-        int flag = 0;
-
-        for(int i = 0 ; i < n ; i++) {
-            if(!arr[i]) {
-                count += 1;
-                sum += (i+1);
-            }
-            if(sum > maxSum) {
-                flag = 1;
-                break;
-            }
-        }
-
-        if(flag) count--;
-        // cout << k << " : " << count << endl;
-        return count >= k;
-
-    }
     int maxCount(vector<int>& banned, int n, int maxSum) {
         int m = banned.size();
-        arr = vector<bool>(n,false);
-        for(int i = 0 ; i < m ; i++) {
-            if(banned[i] <= n) {
-                arr[banned[i]-1] = true;
+        sort(banned.begin(),banned.end());
+        // for(int b : banned) cout << b << "   ";
+        int sum = 0, i = 0, count = 0;
+        for(int k = 1 ; k <= n  && sum + k <= maxSum; k++) {
+            sum += k;
+            count += 1;
+            if(i < m && banned[i] == k) {
+                sum -= k;
+                count -= 1;
+                while(i < m && banned[i] == k) i++;
             }
         }
-
-        int start = 0;
-        int end = n;
-
-        while(start <= end) {
-            int mid = (start + end) >> 1;
-            if(isPossible(banned,n,maxSum,mid)) {
-                start = mid + 1;
-            } else {    
-                end = mid - 1;
-            }
-        }
-        return end;
+        return count;
     }
 };
