cad16bc24c9865aba31ca2c8c24390449cfc4f11
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-04-07 00:34:14 +0530

    Time: 27 ms (45.69%) | Memory: 39.9 MB (42.65%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -7,12 +7,18 @@ public:
         vector<vector<bool>> vis(n,vector<bool>(n,false));
         for(auto road : roads) vis[road[0]][road[1]] = vis[road[1]][road[0]] = true;
 
-        int maxRank = 0;
-        for(int node1 = 0 ; node1 < n ; node1 += 1) {
-            for(int node2 = node1 + 1 ; node2 < n ; node2 += 1) {
-                int totalIndegree = inDegree[node1] + inDegree[node2];
-                if(vis[node1][node2] || vis[node2][node1]) totalIndegree -= 1;
-                maxRank = max(maxRank,totalIndegree);
+        vector<vector<int>> pairs;
+        for(int i = 0 ; i < n ; i++) pairs.push_back({inDegree[i],i});
+        sort(pairs.begin(),pairs.end());
+        int maxIndegree = pairs[n - 1][0], maxRank = 0;
+ 
+        for(int i = n - 1 ; i >= 1 && pairs[i][0] == maxIndegree ; i--) {
+
+            for(int j = i - 1 ; j >= 0 ; j--) {
+                int extra = 0;
+                if(vis[pairs[i][1]][pairs[j][1]] || vis[pairs[j][1]][pairs[i][1]]) 
+                    extra += 1;
+                maxRank = max(maxRank,maxIndegree + pairs[j][0] - extra);
             }
         }
         return maxRank;
