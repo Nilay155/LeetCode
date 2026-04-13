c098c63726e28b378d433fce8b620a7970651564
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-04-13 23:13:40 +0530

    Time: 1138 ms (5.17%) | Memory: 150 MB (72.78%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,66 +1,51 @@
 class Solution {
 private:
-    int mergeArrays(vector<int> &nums,int low,int mid,int high) {
-        vector<int> leftHalf(mid-low+1);
-        vector<int> rightHalf(high-mid);
+    int count(vector<int> &nums,int l,int mid,int r) {
+        
+        vector<int> leftHalf(mid - l + 1), rightHalf(r - mid);
+        for(int k = 0 ; k < mid - l + 1 ; k++) leftHalf[k] = nums[k + l];
+        for(int k = 0 ; k < r - mid ; k++) rightHalf[k] = nums[mid + k + 1];
 
-        int k = 0, l = 0;
-        for(int i = low ; i <= mid; i++) {
-            leftHalf[k++] = nums[i];
-        }
-        for(int i = mid+1 ; i <= high ; i++) {
-            rightHalf[l++] = nums[i];
-        }
+        int pairs = 0;
+        int i = 0, j = 0,n = mid - l + 1, m = r - mid;
+        while(i < n) {
 
-        int i = 0, j = 0, t = 0;
-        vector<int> arr(high-low+1);
-        long long int res = 0;
-
-        while(i < k && j < l) {
-            if(leftHalf[i] < rightHalf[j]) {
-                arr[t++] = rightHalf[j++]; 
-            } else {
-                arr[t++] = leftHalf[i++];
-            }
-        }
-        while(i < k) {
-            arr[t++] = leftHalf[i++];
+            while(j < m && (1LL * leftHalf[i] > 2LL * rightHalf[j])) j += 1;
+            
+            pairs += j;
+            i += 1;
         } 
-        while(j < l) {
-            arr[t++] = rightHalf[j++];
-        }
-
+        
+        int k = l;
         i = 0, j = 0;
-        while(i < k && j < l) {
-            if(leftHalf[i] > (2LL*rightHalf[j])) {
-                res += (l-j);
-                i++;
+        while(i < n && j < m) {
+            if(leftHalf[i] > rightHalf[j]) {
+                nums[k++] = rightHalf[j++];
             } else {
-                j++;
+                nums[k++] = leftHalf[i++];
             }
         }
+        while(i < n) nums[k++] = leftHalf[i++];
+        while(j < m) nums[k++] = rightHalf[j++];
 
-        int z = low;
-        for(int  f= 0 ; f < t ; f++) {
-            nums[z++] = arr[f];
-        }
-        cout << res << endl;
-        return res;
+        return pairs;
     }
-    int divideConquer(vector<int> &nums,int low,int high) {
-        if(low >= high) return 0;
+    int countPairs(vector<int> &nums,int l,int r) {
+        
+        if(l >= r) 
+            return 0;
+        
+        int mid = l + (r - l) / 2;
+        int leftHalf = countPairs(nums,l,mid);
+        int rightHalf = countPairs(nums,mid + 1,r);
+        int countCurrentPairs = count(nums,l,mid,r);
 
-        int mid = (low + high) >> 1;
-        int left = divideConquer(nums,low,mid);
-        int right = divideConquer(nums,mid+1,high);
-        int merge = mergeArrays(nums,low,mid,high);
-
-        return (left + right + merge);
+        return (countCurrentPairs + leftHalf + rightHalf);
     }
 public:
     int reversePairs(vector<int>& nums) {
-        int ans = divideConquer(nums,0,nums.size()-1);
-        // for(int num : nums) cout << num << " : ";
+        int ans = countPairs(nums,0,nums.size() - 1);
+        for(int n : nums) cout << n << " ";
         return ans;
     }
 };
