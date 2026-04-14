74bcaf7c4fe2923c3fbd48fada929b0b8cbbe6c8
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-04-14 22:40:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (92.72%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -15,10 +15,23 @@ private:
         for(int i = 0 ; i < n ; i++)
             for(int j = 0 ; j < n ; j++)
                 matrix[i][j] = ans[i][j];
-        return ;
     }
 public:
     void rotate(vector<vector<int>>& matrix) {
-        imageRotation(matrix);
+        // imageRotation(matrix);
+        int n = matrix.size();
+        for(int i = 0 ; i < n ; i++)
+            reverse(matrix[i].begin(),matrix[i].end());
+        
+        int r = n - 1, c = 0;
+        while(r >= 0 && c < n) {
+            int u = r,v = c;
+            while(u >= 0 && v < n) {
+                swap(matrix[u][c],matrix[r][v]);
+                u -= 1, v += 1;
+            }
+            r -= 1, c += 1;
+        }
+        return ;
     }
 };
