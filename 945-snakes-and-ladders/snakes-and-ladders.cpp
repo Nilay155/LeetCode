48b1c52a38198e4fdaa904ec25395236f7f9491c
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-04-15 01:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (80.82%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,15 +1,29 @@
 class Solution {
+private:
+    pair<int,int> numberToIndexes(int k,int n) {
+        k -= 1;
+        int rr = k / n;
+        int r = n - rr - 1;
+        int c;
+        if(rr % 2 == 0) {
+            int t = k % n;
+            c = t;
+        } else {
+            int t = k % n;
+            c = n - t - 1;
+        }
+        return {r,c};
+    }
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n = board.size();
-        int maxJump = n*n, k = 1;
-        vector<pair<int,int>> indexes(n*n + 1);
-        bool flag = true;
-        for(int i = n - 1  ; i >= 0 ; i--) {
-            if(flag) for(int j = 0 ; j < n ; j++) indexes[k++] = {i,j};
-            else for(int j = n - 1 ; j >= 0 ; j--) indexes[k++] = {i,j};
-            flag = !flag;
-        }
+        // vector<pair<int,int>> indexes(n*n + 1);
+        // bool flag = true;
+        // for(int i = n - 1  ; i >= 0 ; i--) {
+        //     if(flag) for(int j = 0 ; j < n ; j++) indexes[k++] = {i,j};
+        //     else for(int j = n - 1 ; j >= 0 ; j--) indexes[k++] = {i,j};
+        //     flag = !flag;
+        // }
 
         queue<pair<int,int>> q;
         vector<bool> vis(n * n + 1,false);
@@ -23,8 +37,8 @@ public:
             
             for(int jump = 1 ; jump <= 6 && currPosition + jump <= n*n ; jump += 1) {
                 int nextMove = jump + currPosition;
-
-                auto [x,y] = indexes[nextMove];
+                auto [x,y] = numberToIndexes(nextMove,n);
+                // cout << nextMove << "  :  " <<  x << "   :  " << y << "\n"; 
                 if(board[x][y] != -1) {
                     int newPosition = board[x][y];
                     if(vis[newPosition]) continue;
