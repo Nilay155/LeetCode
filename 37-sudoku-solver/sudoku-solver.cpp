0da5490d8010fe932788531b0a70d2a8723d8f9a
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-04-15 11:42:40 +0530

    Time: 219 ms (82.93%) | Memory: 9 MB (9.63%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,33 +1,67 @@
 class Solution {
 private:
-bool check(vector<vector<char>> &board, int i, int j, char val)
-{
-    int row = i - i%3, column = j - j%3;
-    for(int x=0; x<9; x++) if(board[x][j] == val) return false;
-    for(int y=0; y<9; y++) if(board[i][y] == val) return false;
-    for(int x=0; x<3; x++)
-    for(int y=0; y<3; y++)
-        if(board[row+x][column+y] == val) return false;
-    return true;
-}
-private:
-    bool solve(vector<vector<char>> &board,int i,int j) {
-        if(i == 9) return true;
-        
+    vector<vector<bool>> boxes,columns,rows;
+    bool check(vector<vector<char>> &board) {
 
-        for(char ch = '1' ; ch <= '9' ; ch++) {
-            while(j < 9 && board[i][j] != '.') j++;
-            if(j == 9) return solve(board,i+1,0);
-            if(board[i][j] == '.' && check(board,i,j,ch)) {
-                board[i][j] = ch;
-                if(solve(board,i,j+1)) return true;
-                board[i][j] = '.';
-            } 
+        for(int r = 0 ; r < 9 ; r++) {
+            for(int c = 0 ; c < 9 ; c++) {
+                if(board[r][c] == '.')
+                    return false;
+            }
+        }
+        return true;
+    }
+    int getBoxNumber(int r,int c) {
+        return 3 * (r / 3) + c / 3;
+    }
+    bool solve(vector<vector<char>> &board) {
+        if(check(board))
+            return true;
+        
+        for(int r = 0 ; r < 9 ; r++) {
+            for(int c = 0 ; c < 9 ; c++) {
+                if(board[r][c] != '.') continue;
+
+                // board[r][c] == '.'
+                for(char ch = '1' ; ch <= '9' ; ch++) {
+                    if(!columns[c][ch - '0'] && !rows[r][ch -'0'] 
+                    && !boxes[getBoxNumber(r,c)][ch - '0']) {
+
+                        board[r][c] = ch;
+                        columns[c][ch - '0'] = true;
+                        rows[r][ch - '0'] = true;
+                        boxes[getBoxNumber(r,c)][ch - '0'] = true;
+
+                        if(solve(board))
+                            return true;
+
+                        columns[c][ch - '0'] = false;
+                        rows[r][ch - '0'] = false;
+                        boxes[getBoxNumber(r,c)][ch - '0'] = false;
+                        board[r][c] = '.';
+                    }
+                }
+                return false;
+            }
         }
         return false;
     }
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board,0,0);
+        boxes = vector<vector<bool>> (10,vector<bool>(10,false));
+        columns = vector<vector<bool>> (10,vector<bool>(10,false));
+        rows = vector<vector<bool>> (10,vector<bool>(10,false));
+        for(int r = 0 ; r < 9 ; r++) {
+            for(int c = 0 ; c < 9 ; c++) {
+                if(board[r][c] != '.') {
+                    columns[c][board[r][c] - '0'] = true;
+                    rows[r][board[r][c] - '0'] = true;
+                    boxes[getBoxNumber(r,c)][board[r][c] - '0'] = true;
+                    // cout << board[r][c] - '0' << "\n";
+                }
+            }
+        }
+        solve(board);
+        return ;
     }
 };
