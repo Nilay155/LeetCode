2c7b0167ffd01357b508194db383d8158618b120
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-05-05 18:20:02 +0530

    Time: 7 ms (21.60%) | Memory: 18.9 MB (9.52%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
@@ -4,27 +4,40 @@ public:
         int n = arr.size();
         vector<int> nums = arr;
         sort(nums.begin(),nums.end());
-        int i = 0, p = -1, chunks = 0;
+        int chunks = 0, j = 0;
 
+        // O(n ^ 2) approach
+        // for(int i = 0 ; i < n ; i++) {
+            // unordered_map<int,int> freq;
+            // for(int j = p + 1 ; j <= i ; j++) {
+            //     freq[nums[j]] += 1;
+            // }
+            // for(int j = p + 1 ; j <= i ; j++) {
+            //     freq[arr[j]] -= 1;
+            // }
+            // bool flag = true;
+            // for(auto [e,f] : freq) {
+            //     if(f > 0 || f < 0) {
+            //         flag = false;
+            //         break;
+            //     }
+            // }
+            // if(flag) {
+            //     p = i;
+            //     chunks += 1;
+            // }
+        // }
+
+        // O(n) optimization
+        multiset<int> mst;
         for(int i = 0 ; i < n ; i++) {
-            unordered_map<int,int> freq;
-            for(int j = p + 1 ; j <= i ; j++) {
-                freq[nums[j]] += 1;
+            mst.insert(arr[i]);
+            while(j <= i && mst.find(nums[j]) != mst.end()) {
+                auto it = mst.find(nums[j]);
+                mst.erase(it);
+                j += 1;
             }
-            for(int j = p + 1 ; j <= i ; j++) {
-                freq[arr[j]] -= 1;
-            }
-            bool flag = true;
-            for(auto [e,f] : freq) {
-                if(f > 0 || f < 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) {
-                p = i;
-                chunks += 1;
-            }
+            if(j > i) chunks += 1;
         }
         return chunks;
     }
