6c6b3f47771bc34af4d71798b748eff042a78f1d
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-05-25 23:08:46 +0530

    Time: 0 ms (100.00%) | Memory: 95.3 MB (41.10%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
-        int n = A.size(), m = B.size();
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        if(m > n) 
+            return findMedianSortedArrays(nums2,nums1);
+        
+        int half = (m + n + 1) / 2;
 
-        if(n > m) return findMedianSortedArrays(B,A);
-        int total = n + m;
-        int half = total / 2;
-        int l = 0, r = n-1;
-
-        while(true) {
-
-            int mid1 = (l + r) >> 1;
-            int mid2 = half - mid1 - 2;
+        int s = 0, e = min(m,half);
+        while(s <= e) {
+            int mid = s + (e - s) / 2;
+            int k1 = mid;
+            int k2 = half - k1;
 
             int l1 = INT_MIN, l2 = INT_MIN;
             int r1 = INT_MAX, r2 = INT_MAX;
 
-            if(mid1 >= 0 && mid1 < n) l1 = A[mid1];
-            if(mid2 >= 0 && mid2 < m) l2 = B[mid2];
-            if(mid1+1 >= 0 && mid1+1 < n) r1 = A[mid1+1];
-            if(mid2+1 >= 0 && mid2+1 < m) r2 = B[mid2+1];
+            if(k1 - 1 >= 0) l1 = nums1[k1 - 1];
+            if(k1 < m) r1 = nums1[k1];
+            if(k2 - 1 >= 0) l2 = nums2[k2 - 1];
+            if(k2 < n) r2 = nums2[k2];
 
             if(l1 <= r2 && l2 <= r1) {
-                // Correct Partition
-                if(total%2) return double(min(r1,r2)) / 1.00;
-                else return double(min(r1,r2) + max(l1,l2)) / 2.00;
+                // optimal condition
+                if((m + n) & 1) {
+                    // odd length
+                    return (double) (max(l1,l2)) / 1.00;
+                } else {
+                    // even length
+                    return (double) (min(r1,r2) + max(l1,l2)) / 2.00;
+                }
+            } else if(l1 > r2) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
-            else if(l2 > r1) {
-                l = mid1 + 1;
-            } 
-            else {
-                r = mid1 - 1;
-            }
+
         }
-        return -1.0;
+        return 0.00;
     }
 };
