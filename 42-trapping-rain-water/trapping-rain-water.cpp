528d68d0cb8235639635573c7501632d6eb5d864
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-05-31 14:07:49 +0530

    Time: 3 ms (16.58%) | Memory: 26.1 MB (54.97%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if(n == 1) return 0;
+        if(n <= 2) return 0;
 
-        vector<int> leftMax(n,0), rightMax(n,0);
-
-        for(int i = 1 ; i < n ; i++) leftMax[i] = max(leftMax[i - 1],height[i - 1]);
-        for(int i = n - 2 ; i >= 0 ; i--) rightMax[i] = max(rightMax[i + 1],height[i + 1]);
-
+        int s = 1, e = n - 2, leftMax = height[0], rightMax = height[n - 1];
         int ans = 0;
-        for(int i = 0 ; i < n ; i++) {
-            int left = leftMax[i], right = rightMax[i];
-            int mini = min(left,right);
-            int waterLevel = mini - height[i];
-            if(waterLevel >= 0) 
-                ans += waterLevel;
+        while(s <= e) {
+            
+            if(rightMax < leftMax) {
+                int mini = min(leftMax,rightMax);
+                int waterLevel = mini - height[e];
+                if(waterLevel > 0) ans += waterLevel; 
+                rightMax = max(rightMax,height[e]);
+                e--;   
+            } else {
+                int mini = min(leftMax,rightMax);
+                int waterLevel = mini - height[s];
+                if(waterLevel > 0) ans += waterLevel;
+                leftMax = max(leftMax,height[s]);
+                s++;  
+            }
         }
         return ans;
     }
