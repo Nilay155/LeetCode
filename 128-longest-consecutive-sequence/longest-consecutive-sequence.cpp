dda22c997e9eb8452622b7d7dc527cf7af357595
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-06-02 23:17:12 +0530

    Time: 143 ms (9.86%) | Memory: 101.1 MB (5.06%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -2,17 +2,55 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
-        map<int,int> map;
+        int grpNumber = 1, ans = 0;
 
-        for(int num : nums) map[num] = 1;
+        unordered_map<int,int> groups; // grpNumber,grpSize
+        unordered_map<int,int> vis; // element,grpNumber
+        unordered_map<int,int> grpEndNumber; // grpNumber,elementEnding
 
-        int ans = 0;
-        for(auto& it : map) {
-            int prev = it.first - 1;
-            if(map.find(prev) != map.end()) {
-                map[it.first] += map[prev];
+        for(int i = 0 ; i < n ; i++) {
+            int e = nums[i];
+            if(vis.count(e)) continue;
+            
+            int le = e - 1, re = e + 1;
+            bool l = false, r = false;
+            if(vis.count(le)) {
+                l = true;
+                int currGrpNumber = vis[le];
+                groups[currGrpNumber] += 1;
+                vis[e] = currGrpNumber;
+                ans = max(ans,groups[currGrpNumber]);
+                grpEndNumber[currGrpNumber] = e;
+
             }
-            ans = max(ans,map[it.first]);
+            if(vis.count(re)) {
+                r = true;
+                if(l) {
+                    // left , element , right have to be merged
+                    int leftGrpNumber = vis[e];
+                    int rightGrpNumber = vis[re];
+                    int lSize = groups[leftGrpNumber];
+                    int rSize = groups[rightGrpNumber];
+
+                    ans = max(ans,lSize + rSize);
+                    grpEndNumber[leftGrpNumber] = grpEndNumber[rightGrpNumber];
+                    vis[grpEndNumber[leftGrpNumber]] = leftGrpNumber;
+                    groups[leftGrpNumber] = lSize + rSize;
+                    grpEndNumber.erase(rightGrpNumber);
+                    groups.erase(rightGrpNumber);
+                } else {
+                    int currGrpNumber = vis[re];
+                    groups[currGrpNumber] += 1;
+                    vis[e] = currGrpNumber;
+                    ans = max(ans,groups[currGrpNumber]);
+                }
+            }
+            if(!l && !r) {
+                vis[e] = grpNumber;
+                groups[grpNumber] = 1;
+                grpEndNumber[grpNumber++] = e;
+                ans = max(ans,1);
+            }
         }
         return ans;
     }
