ff2de47ba5485df15fa77be7f311c1334b526596
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-06-04 23:10:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (52.32%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,52 +1,52 @@
 class Solution {
 public:
-    bool check(string &p,int j,int m) {
-        
-        int k = j;
-        while(k < m) {
-            if(k+1 < m) {
-                if(p[k+1] != '*') return false;
-                k += 2;
-            } else {
-                return false;
+    bool check(string &p,int j) {
+
+        for(int k = j ; k < p.length() ; k++) {
+            if(p[k] != '*') {
+                if(k + 1 < p.length() && p[k + 1] == '*')
+                    continue;
+                else
+                    return false;
             }
         }
         return true;
     }
-    bool dfs(string &s,string &p,int i,int j,int n,int m) {
-        if(i >= n && j >= m) return true;
-        if(i >= n) return check(p,j,m);
+    int dp[21][21];
+private:
+    bool match(string &s,string &p,int i,int j) {
+        if(i >= s.length()) {
 
-        int option1 = false;
-        int option2 = false;
-        int option3 = false;
-        
-        if(j+1 < m && p[j+1] == '*') {
-            option3 |= dfs(s,p,i,j+2,n,m);
-                for(int k = i ; k < n; k++) {
-                    if(s[k] == p[j] || p[j] == '.') {
-                        option3 |= dfs(s,p,k+1,j+2,n,m);
-                    } else {
-                        break;
-                    }
-                }
-        
+            if(check(p,j)) {
+                return true;
+            } else {
+                return false;
+            }
         }
-        else if(s[i] == p[j]) {
-            option1 = dfs(s,p,i+1,j+1,n,m);
-        } else if(p[j] == '.') {
-            option2 = dfs(s,p,i+1,j+1,n,m);
+
+        if(i >= s.length() || j >= p.length())
+            return false;
+        if(dp[i][j] != -1) 
+            return dp[i][j];
+
+        bool ans = false;
+        if(j + 1 < p.length() && p[j + 1] == '*') {
+            if(s[i] == p[j] || p[j] == '.') {
+                ans = ans | match(s,p,i + 1,j);  
+            } 
+            ans = ans | match(s,p,i,j + 2);
+        } else if(s[i] == p[j]) {
+            ans = ans | match(s,p,i + 1,j + 1);
+        } else if(s[i] != p[j] && p[j] == '.') {
+            ans = ans | match(s,p,i + 1,j + 1);
         } else {
             return false;
         }
-        return option1 || option2 || option3;
+        return dp[i][j] = ans;
     }
+public:
     bool isMatch(string s, string p) {
-        int n = s.length();
-        int m = p.length();
-
-        int i = 0 , j = 0;
-
-        return dfs(s,p,i,j,n,m);
+        memset(dp,-1,sizeof(dp));
+        return match(s,p,0,0);
     }
 };
