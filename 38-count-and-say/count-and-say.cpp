61c993588330c5a98a32f61f70b4c971f1905610
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-06-13 16:46:40 +0530

    Time: 4 ms (48.96%) | Memory: 11.2 MB (11.61%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,29 +1,37 @@
 class Solution {
 private:
-    string countSay(int curr,int end,string s) {
-        
-        string temp;
-        int count = 1, element = s[0]-'0', n = s.length();
-        for(int i = 1 ; i < n ; i++) {
-            if(s[i] == s[i-1]) {
+    string ans;
+    bool f(int k,int &n,string s) {
+        if(k == n) {
+            ans = s;
+            return true;
+        }
+
+        int m = s.length(), count = 0;
+        string newStr = "";
+        char ch;
+
+        for(int r = 0 ; r < m ; r++) {
+            if(count == 0) {
+                ch = s[r];
+                count += 1;
+            } else if(ch == s[r]) {
                 count += 1;
             } else {
-                temp += to_string(count);
-                temp += to_string(element);
-
-                count = 1, element = s[i]-'0';
+                newStr += to_string(count) + ch;
+                ch = s[r];
+                count = 1;
             }
         }
-        
-        temp += to_string(count);
-        temp += to_string(element);
-   
-        if(curr == end) return temp;
-        return countSay(curr + 1,end,temp);
+        newStr += to_string(count) + ch;
+
+        if(f(k + 1,n,newStr))
+            return true;
+        return false;
     }
 public:
     string countAndSay(int n) {
-        if(n == 1) return "1";
-        return countSay(2,n,"1");
+        f(1,n,"1");
+        return ans;
     }
 };
