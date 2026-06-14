b055db722626d42616be609cd219e369fedd2337
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-06-14 18:08:24 +0530

    Time: 54 ms (10.76%) | Memory: 10.5 MB (94.18%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -25,7 +25,26 @@ private:
     }
 public:
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
-        return f(word1,word2,0,0);
+        memset(dp,1e9,sizeof(dp));
+        // return f(word1,word2,0,0);
+        int n = word1.length(), m = word2.length();
+        for(int i = 0 ; i <= n ; i++) 
+            dp[i][0] = i; 
+        for(int j = 0 ; j <= m ; j++)
+            dp[0][j] = j;
+        
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= m ; j++) {
+                dp[i][j] = 1e9;
+                if(word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
+                } else {
+                    dp[i][j] = min(dp[i][j],1 + dp[i - 1][j]);
+                    dp[i][j] = min(dp[i][j],1  + dp[i - 1][j - 1]);
+                    dp[i][j] = min(dp[i][j],1 + dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
