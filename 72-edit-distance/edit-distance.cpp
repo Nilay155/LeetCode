8681a715495b2aa1ac2f643c776381bc4f8d5147
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-06-14 17:48:08 +0530

    Time: 40 ms (13.22%) | Memory: 11.7 MB (86.27%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,50 +1,31 @@
 class Solution {
 private:
     int dp[501][501];
-    int f(string &s,string &t,int i,int j) {
-        if(i == 0) {
-            return j;
+    int f(string &word1,string &word2,int i,int j) {
+        if(i >= word1.length()) {
+            return word2.length() - j;
         }
-        if(j == 0) {
-            return i;
+        if(j >= word2.length()) {
+            return word1.length() - i;
         }
-        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j] != -1)
+            return dp[i][j];
 
-        if(s[i-1] == t[j-1]) return dp[i][j] = f(s,t,i-1,j-1);
-        else {
-            // replace
-            int replace = 1 + f(s,t,i-1,j-1);
-            int deletee = 1 + f(s,t,i-1,j);
-            int insert = 1 + f(s,t,i,j-1);
-
-            return dp[i][j] =  min({replace,insert,deletee});
+        int ans = INT_MAX;
+        if(word1[i] == word2[j]) {
+            ans = min(ans,f(word1,word2,i + 1,j + 1));
+        } else {
+            // word1[i] != word2[j]
+            int op1 = 1 + f(word1,word2,i,j + 1); // insertion
+            int op2 = 1 + f(word1,word2,i + 1,j + 1); // replace
+            int op3 = 1 + f(word1,word2,i + 1,j); // deletion
+            ans = min(ans,min({op1,op2,op3}));
         }
+        return dp[i][j] = ans;
     }
 public:
     int minDistance(string word1, string word2) {
-        int n = word1.length(), m = word2.length();
-        memset(dp,0,sizeof(dp));
-        // return f(word1,word2,n,m);
-
-        for(int j = 0 ; j <= m ; j++) {
-            dp[0][j] = j;
-        }
-        for(int i = 0 ; i <= n ; i++) {
-            dp[i][0] = i;
-        }
-
-        for(int i = 1 ; i <= n ; i++) {
-            for(int j = 1 ; j <= m ; j++) {
-
-                if(word1[i-1] == word2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1];
-                } else {
-                    dp[i][j] = 1 + min({
-                            dp[i-1][j-1],dp[i-1][j],dp[i][j-1]
-                    });
-                }
-            }
-        }
-        return dp[n][m];
+        memset(dp,-1,sizeof(dp));
+        return f(word1,word2,0,0);
     }
 };
