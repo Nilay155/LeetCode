c8a93f0996b8b9c40d7342f646cd11506de8db8c
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-06-14 17:26:14 +0530

    Time: 4 ms (41.00%) | Memory: 11.4 MB (49.73%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,69 +1,58 @@
 class Solution {
-private:
-    string solve(stack<string> &st) {
-
-        vector<string> ans;
-
-        while(!st.empty()) {
-            string str = st.top(); st.pop();
-            if(str == "/" || str == ".") continue;
-            if(str == "..") {
-                // Logic To Handle
-                int count = 0;
-                while(!st.empty() && (st.top() == ". " || st.top() == "..")) {
-                    if(st.top() == "..") count += 1;
-                    st.pop();
-                    
-                }
-                if(!st.empty()) st.pop();
-                while(count--) st.push("..");
-                
-            } else {
-                ans.push_back(str);
-            }
-        }
-        reverse(ans.begin(),ans.end());
-        if(ans.size() == 0) return "/";
-        string res;
-        for(int i = 0 ; i < ans.size(); i++) {
-            res += "/";
-            res += ans[i];
-        }
-        return res;
-    }
 public:
     string simplifyPath(string path) {
-        int n = path.size();
-        int i = 0;
-        stack<string> st;
+        
+        vector<string> data;
+        int n = path.length(), i = 0;
 
         while(i < n) {
-            string temp;
+            string curr;
+            // handling '///' patterns
             while(i < n && path[i] == '/') {
-                i++;
+                curr += path[i];
+                i += 1;
             }
-            while(i < n && path[i] == '.') {
-                temp += path[i];
-                i++;
+            if(!curr.empty())
+                data.push_back("/");
+            
+            curr = "";
+            // handling '...' dots but edge case to be handled
+            bool alpha = false;
+            while(i < n && path[i] != '/') {
+                if(isalpha(path[i]))
+                    alpha = true;
+                curr += path[i];
+                i += 1;
             }
-            if(i < n && path[i] == '/') {
-                if(temp.size() >= 2) st.push(temp);
-                temp.clear();
+            if(!curr.empty()) {
+                if(alpha) {
+                    data.push_back(curr);
+                } else {
+                    if(curr.length() <= 2) {
+                        if(curr.length() == 1)
+                            data.pop_back();
+                        else {
+                            data.pop_back();
+                            if(!data.empty())
+                                data.pop_back();
+                            if(!data.empty())
+                                data.pop_back();
+                        }
+                    } else {
+                        data.push_back(curr);
+                    }
+                }
             }
-
-            while(i < n && path[i] != '/' ) {
-                temp += path[i];
-                i++;
-            }
-            if(temp.size()) st.push(temp);
-            temp.clear();
-
         }
-        // while(!st.empty()) {
-        //     cout << st.top() << "  " ;
-        //     st.pop();
-        // }
-        return solve(st);
-        // return "";
+        if(data.empty())
+            return "/";
+        
+        if(data.back() == "/" && data.size() > 1) 
+            data.pop_back();
+        string result;
+        for(int i = 0 ; i < data.size() ; i++) {
+            result += data[i];
+        }
+        return result;
     }
 };
