b52c474265eabddc741669e7410707247489d91e
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-06-16 23:29:12 +0530

    Time: 2 ms (42.36%) | Memory: 11 MB (28.76%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -1,27 +1,42 @@
 class Solution {
 private:
-    vector<string> ans;
-    void createIpAddresses(string &s,int i,int count,string res) {
-        if(i >= s.length() && count == 0) {
-            ans.push_back(res.substr(0,res.length() - 1));
-            return ;
+    bool f(string &s,int i,vector<string> &ans,string str,int count) {
+        int n = s.length();
+        if(i >= n && count == 0) {
+            str.pop_back();
+            ans.push_back(str);
+            return true;
         }
-        if(count <= 0) return ;
-        string str;
-        for(int j = i ; j < s.length() ; j++) {
-            if((j > i && s[i] == '0') || (j - i + 1 >= 4)
-                || stoi(s.substr(i,j - i + 1)) >= 256) break;
-
-            str += s[j];
-            createIpAddresses(s,j + 1,count - 1,res + str + '.');     
+        if(i >= n || count <= 0)
+            return false;
+        
+        bool res = false;
+        string temp;
+        for(int j = i ; j < min(j + 3,n) ; j++) {
+            temp += s[j];   
+            if(j > i && temp[0] != '0' && stoll(temp) <= 255 && count)
+                res = res | f(s,j + 1,ans,str + temp + ".",count - 1);
+            else if(j == i && count)
+                res = res | f(s,j + 1,ans,str + temp + ".",count - 1);
         }
+        return res;
     }
 public:
     vector<string> restoreIpAddresses(string s) {
         int n = s.length();
-        if(n <= 3 || n >= 13) return {};
-        ans.clear();
-        createIpAddresses(s,0,4,"");
+
+        if(n > 12 || n < 4)
+            return {};
+
+        for(int i = 0 ; i < n ; i++) {
+            if(!isdigit(s[i]))
+                return {};
+        }
+        vector<string> ans;
+        bool flag = f(s,0,ans,"",4);
+        
+        if(!flag)
+            return {};
         return ans;
-    }
+    }   
 };
