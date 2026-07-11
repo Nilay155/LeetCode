0b94643447eaf2e7c9fd8048f69f1f13bde01ac9
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-07-11 23:23:56 +0530

    Time: 392 ms (29.65%) | Memory: 22.2 MB (79.90%) - LeetSync

--- 3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.cpp
+++ 3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.cpp
@@ -1,32 +1,30 @@
 class Solution {
 private:
-    bool check(vector<vector<int>> &grid) {
-        bool flag = true;
+    int f(vector<vector<int>> &grid) {
+
+        bool flag = false;
         for(int i = 0 ; i < 3 ; i++) {
             for(int j = 0 ; j < 3 ; j++) {
-                if(grid[i][j] == 0) {
-                    flag = false;
+                if(!grid[i][j]) {
+                    flag = true;
                     break;
                 }
             }
         }
-        return flag;
-    }
-    int minMoves(vector<vector<int>> &grid) {
-        if(check(grid)) return 0;
-        int mini = 1e9;
-
+        if(!flag)
+            return 0;
+        
+        
+        int ans = INT_MAX;
         for(int i = 0 ; i < 3 ; i++) {
             for(int j = 0 ; j < 3 ; j++) {
-                if(grid[i][j] == 0) {
+                if(!grid[i][j]) {
                     for(int u = 0 ; u < 3 ; u++) {
                         for(int v = 0 ; v < 3 ; v++) {
                             if(grid[u][v] > 1) {
-                                int cost = abs(u - i) + abs(v - j);
-
-                                grid[i][j] += 1;
                                 grid[u][v] -= 1;
-                                mini = min(mini, cost + minMoves(grid));
+                                grid[i][j] += 1;
+                                ans = min(ans,abs(i - u) + abs(j - v) + f(grid));
                                 grid[u][v] += 1;
                                 grid[i][j] -= 1;
                             }
@@ -35,10 +33,10 @@ private:
                 }
             }
         }
-        return mini;
+        return ans;
     }
 public:
     int minimumMoves(vector<vector<int>>& grid) {
-        return minMoves(grid);
+        return f(grid);
     }
 };
