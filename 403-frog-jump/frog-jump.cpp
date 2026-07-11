164daa0127c612c246407edff9df0257ff3843ab
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-07-12 01:27:08 +0530

    Time: 66 ms (79.72%) | Memory: 34.5 MB (73.16%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,13 +1,15 @@
 class Solution {
 private:
     unordered_map<int,int> vis;
-    unordered_map<int,unordered_map<int,int>> dp;
+    int dp[2001][2001];
     bool f(vector<int> &stones,int i,int k,int &n) {
         if(i >= n-1)
             return true;
         if(k <= 0)
             return false;
-        if(dp.find(i) != dp.end() && dp[i].find(k) != dp[i].end())
+        // if(dp.find(i) != dp.end() && dp[i].find(k) != dp[i].end())
+        //     return dp[i][k];
+        if(dp[i][k] != -1)
             return dp[i][k];
 
         int jump1 = stones[i] + k, 
@@ -40,6 +42,7 @@ public:
         vis.clear();
         for(int i = 2 ; i < n ; i++) 
             vis[stones[i]] = i;
+        memset(dp,-1,sizeof(dp));
         return f(stones,i,1,n);
     }
 };
