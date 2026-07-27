1353bcfc1c6ecd6633101a882f41a569d98c6c97
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-07-28 00:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (24.56%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -1,51 +1,50 @@
 class Solution {
 private:
-    double bfs(unordered_map<string,set<pair<string,double>>> &adjList,string &start,string &end) {
-        queue<pair<string,double>> q;
-        unordered_set<string> vis;
+    double bfs(string &source,string &destination,unordered_map<string,vector<pair<string,double>>> &adjList) {
 
-        q.push({start,1.0});
-        vis.insert(start);
+        queue<pair<string,double>> q;
+        q.push({source,1.00});
+        unordered_map<string,bool> vis;
+        vis[source] = true;
 
         while(!q.empty()) {
+            auto [front,value] = q.front(); q.pop();
 
-            string node = q.front().first;
-            double value = q.front().second;
-            q.pop();
+            if(front == destination)
+                return value;
             
-            if(!adjList.count(node)) continue;
-            if(node == end) return value;
-
-            for(auto& p : adjList[node]) {
-                
-                if(!vis.count(p.first)) {
-                    q.push({p.first,value*p.second});
-                    vis.insert(p.first);
+            for(auto [nbr,cost] : adjList[front]) {
+                if(!vis[nbr])  {
+                    vis[nbr] = true;
+                    q.push({nbr,cost * value});
                 }
             }
         }
         return -1.0;
-
-
     }
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         
+        unordered_map<string,vector<pair<string,double>>> adjList;
+        unordered_map<string,bool> vis;
         int n = equations.size();
-        int m = queries.size();
-
-        unordered_map<string,set<pair<string,double>>> adjList;
-
         for(int i = 0 ; i < n ; i++) {
-            string ai = equations[i][0];
-            string bi = equations[i][1];
-
-            adjList[ai].insert({bi,values[i]});
-            adjList[bi].insert({ai,1.0/values[i]});
+            adjList[equations[i][0]].push_back({equations[i][1],values[i]});
+            adjList[equations[i][1]].push_back({equations[i][0],1.00/values[i]});
+            vis[equations[i][0]] = vis[equations[i][1]] = true;
         }
-        vector<double> ans;
+
+        int m = queries.size();
+        vector<double> ans(m,-1.00);
         for(int i = 0 ; i < m ; i++) {
-            ans.push_back(bfs(adjList,queries[i][0],queries[i][1]));
+
+            string c = queries[i][0];
+            string d = queries[i][1];
+            if(!vis.count(d) || !vis.count(c))
+                continue;
+            ans[i] = bfs(c,d,adjList);
+            if(ans[i] == -1.00)
+                ans[i] = 1.00 / (bfs(d,c,adjList));
         }
         return ans;
     }
