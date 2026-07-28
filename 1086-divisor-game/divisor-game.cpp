daa92afe4eab944617d1294a037bc9e82f7cc50c
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-07-28 23:47:46 +0530

    Time: 11 ms (1.61%) | Memory: 7.9 MB (10.35%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,9 +1,29 @@
 class Solution {
+private:
+    int dp[1001][3];
+    bool f(int n,int turn) {
+        if(dp[n][turn] != -1)
+            return dp[n][turn];
+
+        bool chance = false, ans1 = false,ans2 = true;
+        for(int x = 1 ; x < n ; x++) {
+            if(n % x == 0) {
+                bool rec = f(n - x,turn == 1 ? 2 : 1);
+                ans1 = ans1 | rec;
+                ans2 = ans2 & rec;
+                chance = true;
+            }
+        }
+
+        if(!chance)
+            return turn == 1 ? false : true;
+
+        
+        return dp[n][turn] = (turn == 1 ? ans1 : ans2);
+    }
 public:
     bool divisorGame(int n) {
-        if(n&1){
-            return false;
-        }
-        return true;
+        memset(dp,-1,sizeof(dp));
+        return f(n,1);
     }
 };
