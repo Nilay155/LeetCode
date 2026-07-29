6806001577c5863f0647c3ffe9912cb804f609a1
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-07-30 00:31:27 +0530

    Time: 2 ms (15.55%) | Memory: 8.8 MB (5.15%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -17,7 +17,23 @@ private:
     }
 public:
     int numTrees(int n) {
-        memset(dp,-1,sizeof(dp));
-        return f(1,n);
+        // memset(dp,-1,sizeof(dp));
+        // return f(1,n);
+
+        vector<vector<int>> dp(n + 1,vector<int>(n + 1,1));
+
+        for(int len = 2 ; len <= n ; len++) {
+            for(int k = 1 ; k + len - 1 <= n ; k++) {
+                int ans = 0;
+                int l = k, r = k + len - 1;
+                for(int x = l ; x <= r ; x++) {
+                    int left = (x - 1 >= 1 ? dp[l][x - 1] : 1);
+                    int right = (x + 1 <= n ? dp[x + 1][r] : 1);
+                    ans += (left * right);
+                }
+                dp[l][r] = ans;
+            }
+        }
+        return dp[1][n];
     }
 };
