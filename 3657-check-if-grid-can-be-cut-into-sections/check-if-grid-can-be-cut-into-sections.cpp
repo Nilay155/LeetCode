b7bc42d9f3c239691929ab83d111c2db372ee3b8
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-08-04 01:31:12 +0530

    Time: 139 ms (71.78%) | Memory: 185.6 MB (97.51%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
@@ -1,58 +1,43 @@
 class Solution {
-private:
-    void mergeIntervals(vector<vector<int>> &intervals,int &n) {
-        int end = intervals[0][1];
-        vector<vector<int>> temp;
-        for(int i = 1; i < n ; i++) {
-            int start = intervals[i][0];
-        }
-    }
 public:
     bool checkValidCuts(int m, vector<vector<int>>& rectangles) {
         int n = rectangles.size();
-        vector<vector<int>> vertical,horizontal;
-        // veritical -> (x-axis)
-        // horizontal -> (y-axis)
 
-        for(auto rectangle : rectangles) {
-            vertical.push_back({rectangle[0],rectangle[2]});
-            horizontal.push_back({rectangle[1],rectangle[3]});
-        }
-        sort(vertical.begin(),vertical.end());
-        sort(horizontal.begin(),horizontal.end());
+        sort(rectangles.begin(),rectangles.end(),[&](vector<int> &A,vector<int> &B) {
+            
+            if(A[1] == B[1]) 
+                return A[3] < B[3];
+            return A[1] < B[1];
+        });
 
-        int cnt = 1;
-        int end = vertical[0][1];
+        int countY = 0, countX = 0;
+
+        int sy = rectangles[0][1], ey = rectangles[0][3];
         for(int i = 1 ; i < n ; i++) {
-            int start = vertical[i][0];
 
-            if(end <= start) {
-                cnt += 1;
-            } 
-            //else {
-            //     if(vertical[i][1] > end)
-            //         cnt = 1;
-            // }
-            end = max(end,vertical[i][1]);
-        }
-        if(cnt >= 3) return true;
+            int nsy = rectangles[i][1], ney = rectangles[i][3];
 
-        cnt = 1;
-        end = horizontal[0][1];
-        // cout << horizontal[0][0] << " : " << horizontal[0][1] << " : " << cnt << endl;
-        for(int i = 1 ; i < n ; i++) {
-            int start = horizontal[i][0];
-
-            if(end <= start) {
-                cnt += 1;
+            if(nsy >= ey) {
+                countY += 1;
             }
-            // else {
-            //     // Collision - merge
-
-            // }
-            // cout << horizontal[i][0] << " : " << horizontal[i][1] << " : " << cnt << endl;
-            end = max(end,horizontal[i][1]);
+            sy = nsy, ey = max(ney,ey);
         }
-        return cnt >= 3;
+
+        sort(rectangles.begin(),rectangles.end(),[&] (vector<int> &A,vector<int> &B) {
+
+            if(A[0] == B[0])
+                return A[2] < B[2];
+            return A[0] < B[0];
+        });
+
+        int sx = rectangles[0][0], ex = rectangles[0][2];
+        for(int i = 1 ; i < n ; i++) {
+            int nsx = rectangles[i][0], nex = rectangles[i][2];
+            if(nsx >= ex) {
+                countX += 1;
+            } 
+            sx = nsx, ex = max(ex,nex);
+        }
+        return (countX >= 2 || countY >= 2);
     }
 };
