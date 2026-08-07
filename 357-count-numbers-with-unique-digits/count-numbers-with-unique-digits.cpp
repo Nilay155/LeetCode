61b4dd87cfbf6540d6c32499f71ab94f6617ba6d
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-08-08 00:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (46.13%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
-        if(n == 0) return 1;
-        if(n == 1) return 10;
+        if(n == 0)
+            return 1;
+        int ans = 10;
         
-        int num = pow(10,n);
-        long long res = 0;
+        for(int i = 2 ; i <= n ; i++) {
 
-        vector<int> dp(num+1,0);
-        for(int i = 0 ; i <= 9 ; i++) dp[i] = 1;
-
-        for(int i = 10 ; i <= num ; i++) {
-            
-            int temp = i/10;
-            int lastDigit = i%10;
-            int flag = 0;
-
-            while(temp) {
-                int digit = temp%10;
-                if(digit == lastDigit) {
-                    flag = 1;
-                    break;
-                }
-                temp /= 10;
+            int k = 10, j = i, mul = 1;
+            while(j--) {
+                mul *= (k >= 10 ? 9 : k);
+                k--;
             }
-            if(!flag && dp[i/10] == 1) {
-                // cout << i << "   " ;
-                dp[i] = 1;
-                res += 1;
-            }
+            ans += mul;
         }
-        return res+10;
+        return ans;
     }
 };
