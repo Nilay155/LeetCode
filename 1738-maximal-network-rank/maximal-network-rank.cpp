730eea9722eddcee507a0c2f64d0ebd9454396ea
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-08-08 02:34:13 +0530

    Time: 21 ms (49.19%) | Memory: 39.3 MB (40.38%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<int> inDegree(n,0);
+        
+        vector<int> inDegree(n);
         for(auto road : roads) inDegree[road[0]] += 1, inDegree[road[1]] += 1;
 
-        vector<vector<bool>> vis(n,vector<bool>(n,false));
-        for(auto road : roads) vis[road[0]][road[1]] = vis[road[1]][road[0]] = true;
+        vector<vector<bool>> vis(n,vector<bool> (n,false));
+        for(auto road : roads) {
+            int u = road[0], v = road[1];
+            vis[u][v] = vis[v][u] = true;
+        }
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                if(i != j) {
+                    int k1 = inDegree[i], k2 = inDegree[j];
 
-        vector<vector<int>> pairs;
-        for(int i = 0 ; i < n ; i++) pairs.push_back({inDegree[i],i});
-        sort(pairs.begin(),pairs.end());
-        int maxIndegree = pairs[n - 1][0], maxRank = 0;
- 
-        for(int i = n - 1 ; i >= 1 && pairs[i][0] == maxIndegree ; i--) {
-
-            for(int j = i - 1 ; j >= 0 ; j--) {
-                int extra = 0;
-                if(vis[pairs[i][1]][pairs[j][1]] || vis[pairs[j][1]][pairs[i][1]]) 
-                    extra += 1;
-                maxRank = max(maxRank,maxIndegree + pairs[j][0] - extra);
+                    if(vis[i][j]) 
+                        ans = max(ans,k1 + k2 - 1);
+                    else
+                        ans = max(ans,k1 + k2);
+                }
             }
         }
-        return maxRank;
+        return ans;
     }
 };
