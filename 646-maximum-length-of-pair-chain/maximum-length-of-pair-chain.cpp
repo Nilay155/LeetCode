a438977babf5b437b674e2e21065adf122d6e970
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-08-09 20:25:58 +0530

    Time: 4 ms (76.74%) | Memory: 26.5 MB (66.36%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -3,24 +3,22 @@ public:
     int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
         sort(pairs.begin(),pairs.end(),[&](vector<int> &A,vector<int> &B) {
-            if(A[0] == B[0])
-                return A[1] < B[1];
-            return A[0] < B[0];
+            if(A[1] == B[1]) 
+                return A[0] < B[0];
+            return A[1] < B[1];
         });
 
-        vector<int> dp(n,1);
+        int end = pairs[0][1];
+        int count = 1;
+
         for(int i = 1 ; i < n ; i++) {
-            for(int j = i - 1 ; j >= 0 ; j--) {
-                if(pairs[i][0] > pairs[j][1]) {
-                    dp[i] = max(dp[i],1 + dp[j]);
-                }
+            int newStart = pairs[i][0], newEnd = pairs[i][1];
+
+            if(newStart > end) {
+                count += 1;
+                end = newEnd;
             }
         }
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++) {
-            ans = max(ans,dp[i]);
-            // cout << pairs[i][0] << " : " << pairs[i][1] << "\n";
-        }
-        return ans;
+        return count;
     }
 };
