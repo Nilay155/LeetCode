96c189a277c8ad690f2a47962b7d96d25ed67c5a
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-08-12 08:38:43 +0530

    Time: 1315 ms (5.83%) | Memory: 49.3 MB (39.81%) - LeetSync

--- 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int minTrioDegree(int n, vector<vector<int>>& edges) {
-        vector<int> inDegree(n + 1, 0);
-        for(auto edge : edges) inDegree[edge[0]] += 1, inDegree[edge[1]] += 1;
-
-        vector<vector<bool>> connected(n + 1,vector<bool> (n + 1,false));
-        for(auto edge : edges) 
-            connected[edge[0]][edge[1]] = connected[edge[1]][edge[0]] = true;
         
-        int minDegree = 1e9;
-        for(int node1 = 1 ; node1 <= n ; node1 += 1) {
-            for(int node2 = node1 + 1 ; node2 <= n ; node2 += 1) {
-                for(int node3 = node2 + 1; node3 <= n ; node3 += 1) {
+        vector<vector<bool>> dp(n + 1,vector<bool>(n + 1,false));
+        vector<int> inDegree(n + 1,0);
+        for(auto edge : edges) {
+            dp[edge[0]][edge[1]] = true;
+            dp[edge[1]][edge[0]] = true;
+            inDegree[edge[0]] += 1;
+            inDegree[edge[1]] += 1;
+        }
 
-                    bool k1 = connected[node1][node2];
-                    bool k2 = connected[node2][node3];
-                    bool k3 = connected[node1][node3];
-
+        int mini = INT_MAX;
+        for(int x = 1 ; x <= n ; x++) {
+            for(int y = 1 ; y <= n ; y++) {
+                if(x == y) continue;
+                for(int z = 1 ; z <= n ; z++) {
+                    if(y == z || x == z) continue;
+                    bool k1 = dp[x][y], k2 = dp[y][z] , k3 = dp[x][z];
                     if(k1 && k2 && k3) {
-                        // Potential-Trio
-                        int totalIndegree = inDegree[node1] + inDegree[node2] + inDegree[node3] - 6;
-                        minDegree = min(minDegree,totalIndegree);
+                        int res = inDegree[x] + inDegree[y] + inDegree[z];
+                        mini = min(mini,res - 6);
                     }
                 }
             }
         }
-        return minDegree == 1e9 ? -1 : minDegree;
+        return mini == INT_MAX ? -1 : mini;
     }
 };
