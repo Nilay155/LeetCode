8400608db157b4fd66897c88afc7903a7b76bfb0
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-08-12 08:39:43 +0530

    Time: 655 ms (16.51%) | Memory: 49.2 MB (40.45%) - LeetSync

--- 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -14,7 +14,7 @@ public:
         int mini = INT_MAX;
         for(int x = 1 ; x <= n ; x++) {
             for(int y = 1 ; y <= n ; y++) {
-                if(x == y) continue;
+                if(x == y || !dp[x][y]) continue;
                 for(int z = 1 ; z <= n ; z++) {
                     if(y == z || x == z) continue;
                     bool k1 = dp[x][y], k2 = dp[y][z] , k3 = dp[x][z];
