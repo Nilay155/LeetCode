8eca451f4032495d32bad8f93fded5348d927c50
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-08-18 09:46:19 +0530

    Time: 13 ms (36.15%) | Memory: 32.6 MB (29.11%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,52 +1,48 @@
 class Solution {
 private:
-    bool isUpper(int x1,int y1,int x2,int y2) {
-        if(x1 == x2) return true;
-        else if((x2 < x1 && y1 <= y2)) return true;
-        else return false;
-    }
-    bool checkPoints(vector<vector<int>> &points,int x1,int y1,int x2,int y2) {
-        // verify if any other point exists in the rectangle or on the line
-        int ubx = max(x1,x2), lbx = min(x1,x2);
-        int uby = max(y1,y2), lby = min(y1,y2);
+    bool check(vector<vector<int>> &points,int i,int j,int f) {
+        int n = points.size();
+        int x1 = points[i][0], y1 = points[i][1];
+        int x2 = points[j][0], y2 = points[j][1];
+        
+        if(f == j) {
+            x1 = points[j][0], y1 = points[j][1];
+            x2 = points[i][0], y2 = points[i][1];
+        }
 
-        for(int i = 0 ; i < points.size() ; i++) {
+        for(int k = 0 ; k < n ; k++) {
+            if(i == k || j == k) continue;
+            int x3 = points[k][0], y3 = points[k][1];
 
-            int x = points[i][0], y = points[i][1];
-
-            if(x == x1 && y == y1) continue;
-            if(x == x2 && y == y2) continue;
-
-            if((x <= ubx && x >= lbx) && (y <= uby && y >= lby)) return false;
+            if((x3 >= x1 && x3 <= x2) && (y3 >= y2 && y3 <= y1))
+                return false;
         }
         return true;
     }
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
-        int ans = 0;
-        // 1. sorting to ensure left-side
-        sort(points.begin(),points.end());
-
-        // 2. checking for every point
-
-        // O(n)
+        int count = 0;
         for(int i = 0 ; i < n ; i++) {
             int x1 = points[i][0], y1 = points[i][1];
-            // checking the left-side points
-            // O(n)
-            for(int j = i-1 ; j >= 0 ; j--) {
+            for(int j = i + 1 ; j < n ; j++) {
                 int x2 = points[j][0], y2 = points[j][1];
-                // O(1)
-                if(isUpper(x1,y1,x2,y2)) {
-                    // O(n)
-                    if(checkPoints(points,x1,y1,x2,y2)) {
-                        ans += 1;
-                    }
+                
+                if(x1 <= x2 && y1 > y2) {
+                    if(check(points,i,j,i))
+                        count += 1;
+                } else if(x1 < x2 && y1 >= y2) {
+                    if(check(points,i,j,i))
+                        count += 1;
+                } else if(x2 <= x1 && y2 > y1) {
+                    if(check(points,i,j,j))
+                        count += 1;
+                } else if(x2 < x1 && y2 >= y1) {
+                    if(check(points,i,j,j))
+                        count += 1;
                 }
             }
         }
-        // T.C. -> O(n*n*n), S.C -> O(1)
-        return ans;
+        return count;
     }
 };
