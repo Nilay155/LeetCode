ac5e7ce566e90548ef68a08a5829ad6c65008e0a
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-08-27 09:45:46 +0530

    Time: 129 ms (48.31%) | Memory: 184.3 MB (59.70%) - LeetSync

--- 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.cpp
+++ 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.cpp
@@ -1,62 +1,43 @@
-typedef long long ll;
+using ll = long long;
 class Solution {
-private:
-    int findIndex(vector<vector<int>> &arr,int pos) {
-        int s = 0, e = pos - 1;
-
-        while(s <= e) {
-            int mid = s + (e-s)/2;
-            if(arr[mid][0] > arr[pos][0]) {
-                e = mid - 1;
-            } else {
-                s = mid + 1;
-            }
-        }
-        return e;
-    }
 public:
     vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
-        int n = nums2.size();
+        int n = nums1.size();
+        vector<pair<int,int>> arr;
 
-        vector<vector<int>> arr;
-        for(int i = 0 ; i < n ; i++) arr.push_back({nums1[i],i});
-        
-        sort(arr.begin(),arr.end());
-        
+        for(int i = 0 ; i < n ; i++)
+            arr.push_back({nums1[i],i});  
+        sort(arr.rbegin(),arr.rend());
+
         vector<int> nums(n);
-        for(int i = 0 ; i < n ; i++) nums[i] = nums2[arr[i][1]];
+        for(int i = 0 ; i < n ; i++) {
+            nums[i] = nums2[arr[i].second];
+        }
 
-        ll sum = 0;
-        priority_queue<int,vector<int>,greater<int>> pq;
-
-        int l = 0, r = 0;
+        ll kSum = 0;
         vector<ll> ans(n);
-
-        while(r < n) {
-            // cout << sum <<"\n";
-            while(r + 1 < n && arr[r][0] == arr[r+1][0]) {
-                ans[arr[r][1]] = sum;
-                r += 1;
-            }
-            ans[arr[r][1]] = sum;
-            while(l <= r) {
-
-                if(pq.size() >= k) {
-                    int curr = pq.top(); 
-                    if(curr < nums[l]) {
-                        pq.pop();
-                        sum -= curr;
-                        sum += nums[l];
-                        pq.push(nums[l]);
-                    }
+        int j = n - 1;
+        priority_queue<int,vector<int>,greater<int>> minHeap;
+        
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            while(i < j && arr[i].first > arr[j].first) {
+                if(minHeap.size() < k) {
+                    kSum += nums[j];
+                    minHeap.push(nums[j]);
                 } else {
-                    sum += nums[l];
-                    pq.push(nums[l]);
+                    // count >= k
+                    int top = minHeap.top();
+                    if(nums[j] >= top) {
+                        minHeap.pop();
+                        kSum -= top;
+                        minHeap.push(nums[j]);
+                        kSum += nums[j];
+                    }
                 }
-                l += 1;
+                j--;
             }
-            r += 1;
-        } 
+            ans[arr[i].second] = kSum;
+        }
         return ans;
     }
 };
