75cb8b951cea75f1b8b8e040de8ce769ba270d77
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-09-11 17:33:57 +0530

    Time: 3 ms (40.25%) | Memory: 13.1 MB (6.16%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    int longestValidParentheses(string str) {
-        int n = str.length();
+    int longestValidParentheses(string s) {
+        int n = s.length();
 
-        vector<pair<int,int>> pairs;
         stack<int> st;
+        vector<int> bracket(n,-1);
+
         for(int i = 0 ; i < n ; i++) {
-            if(str[i] == ')' && !st.empty()) {
-                pairs.push_back({st.top(),i});
-                st.pop();
-            } else if(str[i] == '(') {
+            char ch = s[i];
+            if(ch == '(') {
                 st.push(i);
-            }
-        }
-        sort(pairs.begin(),pairs.end());
-        if(pairs.empty())
-            return 0;
-
-        vector<pair<int,int>> result;
-        int s = pairs[0].first, e = pairs[0].second;
-        for(int i = 1 ; i < pairs.size() ; i++) {
-            int ns = pairs[i].first, ne = pairs[i].second;
-            if(ns <= e + 1) {
-                e = max(e,ne);
             } else {
-                result.push_back({s,e});
-                s = ns,e = ne;
+                if(!st.empty())
+                    bracket[i] = st.top(), st.pop();
             }
         }
-        result.push_back({s,e});
-
         int ans = 0;
-        for(int i = 0 ; i < result.size() ; i++) {
-            ans = max(ans,result[i].second - result[i].first + 1);
+        for(int i = 0 ; i < n ; i++) {
+            if(bracket[i] != -1) {
+                int prev = bracket[i];
+                if(prev - 1 >= 0 && bracket[prev - 1] != -1) {
+                    bracket[i] = bracket[prev - 1];
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            if(bracket[i] != -1) {
+                ans = max(ans,i - bracket[i] + 1);
+            }
         }
         return ans;
     }
