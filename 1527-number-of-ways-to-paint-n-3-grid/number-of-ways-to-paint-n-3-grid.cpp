3a3fc20ff1df0f113745d4ea1da742a72212d1d4
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-09-13 13:11:17 +0530

    Time: 39 ms (36.08%) | Memory: 9.6 MB (47.19%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -26,7 +26,52 @@ public:
             {2,1,3},{2,3,1},{2,1,2},{2,3,2},
             {3,2,1},{3,1,2},{3,1,3},{3,2,3}
         };
-        memset(dp,-1,sizeof(dp));
-        return f(n,colorCombinations,0,0,0);
+        memset(dp,0,sizeof(dp));
+        // return f(n,colorCombinations,0,0,0);
+
+        // for(int x = 1 ; x <= 3 ; x++)
+        //     for(int y = 1 ; y <= 3 ; y++)
+        //         for(int z = 1 ; z <= 3 ; z++)
+        //             dp[0][x][y][z] = 1;
+        
+        for(int i = 0 ; i < (int) colorCombinations.size() ; i++) {
+
+            int nx = colorCombinations[i][0],
+                ny = colorCombinations[i][1],
+                nz = colorCombinations[i][2];
+
+            dp[1][nx][ny][nz] = 1;
+        }
+        for(int k = 2 ; k <= n ; k++) {
+            for(int x = 1 ; x <= 3 ; x++) {
+                for(int y = 1 ; y <= 3 ; y++) {
+                    for(int z = 1 ; z <= 3 ; z++) {
+                        if(x == y || y == z)
+                            continue;
+                        int ans = 0;
+                        for(int i = 0 ; i < (int) colorCombinations.size() ; i++) {
+
+                            int nx = colorCombinations[i][0],
+                                ny = colorCombinations[i][1],
+                                nz = colorCombinations[i][2];
+
+                            if((k == 1) || (nx != x && ny != y && nz != z)) {
+                                ans = (ans + dp[k - 1][nx][ny][nz]) % MOD;
+                            }
+                        }
+                        dp[k][x][y][z] = ans % MOD;
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0 ; i < (int) colorCombinations.size() ; i++) {
+            int nx = colorCombinations[i][0],
+                ny = colorCombinations[i][1],
+                nz = colorCombinations[i][2];
+
+            ans = (ans + dp[n][nx][ny][nz]) % MOD;
+        }
+        return ans;
     }
 };
