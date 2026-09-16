b4de547cb5131f986852e95ad77ca2897c85c312
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-09-16 17:22:00 +0530

    Time: 55 ms (26.00%) | Memory: 66.3 MB (20.92%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,42 +1,38 @@
 class Solution {
+private:
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        
         int n = graph.size();
-        
         vector<vector<int>> adjList(n);
-        for(int i = 0 ; i < n ; i++) {
-            for(int node : graph[i]) {
-                adjList[node].push_back(i);
-            }
-        }
-        unordered_map<int,int> outDegree;
+        vector<int> inDegree(n,0);
 
         for(int i = 0 ; i < n ; i++) {
-            outDegree[i] = graph[i].size();
+            for(int j = 0 ; j < (int) graph[i].size() ; j++) {
+                adjList[graph[i][j]].push_back(i);
+                inDegree[i]++;
+            }
         }
 
         queue<int> q;
         for(int i = 0 ; i < n ; i++) {
-            if(outDegree[i] == 0) {
+            if(inDegree[i] == 0)
                 q.push(i);
-            }
         }
-        // cout << q.size() << endl;
-        vector<int> ans;
-        while(!q.empty()) {
-            int top = q.front();
-            q.pop();
-            ans.push_back(top);
 
-            for(int child : adjList[top]) {
-                outDegree[child]--;
-                if(outDegree[child] == 0) {
-                    q.push(child);
-                }
+        vector<int> ans;
+
+        while(!q.empty()) {
+            int node = q.front(); q.pop();
+            ans.push_back(node);
+            for(int nbr : adjList[node]) {
+                inDegree[nbr]--;
+                if(inDegree[nbr] == 0)
+                    q.push(nbr);
             }
         }
         sort(ans.begin(),ans.end());
         return ans;
+
+
     }
 };
