6ff1172c0160565b8a4c1ef714b81642f146839f
Nilay155 <133687328+Nilay155@users.noreply.github.com>
2026-09-23 09:45:39 +0530

    Time: 35 ms (46.99%) | Memory: 31.4 MB (72.83%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,26 +1,39 @@
 class Solution {
+private:
+    // int dp[501][501];
+    int f(string s,int l,int r,vector<vector<int>> &dp) {
+        if(l >= r)
+            return 0;
+
+        if(dp[l][r] != -1)
+            return dp[l][r];
+
+        if(s[l] == s[r])
+            return dp[l][r] = f(s,l + 1, r - 1,dp);
+        else
+            return dp[l][r] = min(1 + f(s,l + 1,r,dp),1 + f(s,l,r - 1,dp));
+    } 
 public:
     int minInsertions(string s) {
-
         int n = s.length();
-
-        string s1 = s;
-        reverse(s.begin(),s.end());
-        string s2 = s;
-
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-
-        for(int i = 1 ; i <= n ; i++) {
-            for(int j = 1; j <= n ; j++) {
-
-                if(s1[i-1] == s2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-                }
+        // memset(dp,-1,sizeof(dp));
+        vector<vector<int>> dp(n,vector<int>(n,1e9));
+        for(int l = 0 ; l < n ; l++)
+            for(int r = 0 ; r < n ; r++)
+                if(l >= r)
+                    dp[l][r] = 0;
+                
+        for(int l = n - 2 ; l >= 0 ; l--) {
+            for(int r = 1 ; r < n ; r++) {
+                if(l >= r)
+                    continue;
+                    
+                if(s[l] == s[r])
+                    dp[l][r] = dp[l + 1][r - 1];
+                else
+                    dp[l][r] = 1 + min(dp[l + 1][r],dp[l][r - 1]);
             }
-        }   
-
-        return n-dp[n][n];
+        }
+        return dp[0][n - 1];
     }
 };
